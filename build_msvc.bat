8f421c3212a79b2ed01df8a91dc15ea40e66f282
lriki <lriki.net@gmail.com>
2015-10-03 00:59:34 +0900

    binding

--- build_msvc.bat
+++ build_msvc.bat
@@ -6,10 +6,10 @@ mkdir ".\build_vs2013_u"
 cd "build_vs2013_u"
 cmake ".." -G "Visual Studio 12" -DLN_USE_UNICODE_CHAR_SET=ON
 
-MSBuild ".\Lumino.sln" /t:Rebuild /p:Configuration="Debug" /p:Platform="Win32"
+MSBuild ".\Lumino.sln" /t:Rebuild /p:Configuration="Debug" /p:Platform="Win32" /m
 if "%ERRORLEVEL%" NEQ "0" goto BUILD_FAILED
 
-MSBuild ".\Lumino.sln" /t:Rebuild /p:Configuration="Release" /p:Platform="Win32"
+MSBuild ".\Lumino.sln" /t:Rebuild /p:Configuration="Release" /p:Platform="Win32" /m
 if "%ERRORLEVEL%" NEQ "0" goto BUILD_FAILED
 
 cd ".."
@@ -18,10 +18,10 @@ mkdir ".\build_vs2013_mb"
 cd "build_vs2013_mb"
 cmake ".." -G "Visual Studio 12" -DLN_USE_UNICODE_CHAR_SET=OFF
 
-MSBuild ".\Lumino.sln" /t:Rebuild /p:Configuration="Debug" /p:Platform="Win32"
+MSBuild ".\Lumino.sln" /t:Rebuild /p:Configuration="Debug" /p:Platform="Win32" /m
 if "%ERRORLEVEL%" NEQ "0" goto BUILD_FAILED
 
-MSBuild ".\Lumino.sln" /t:Rebuild /p:Configuration="Release" /p:Platform="Win32"
+MSBuild ".\Lumino.sln" /t:Rebuild /p:Configuration="Release" /p:Platform="Win32" /m
 if "%ERRORLEVEL%" NEQ "0" goto BUILD_FAILED
 
 cd ".."
