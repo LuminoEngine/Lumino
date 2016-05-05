8396c50842e0ed68266c099bcd8deafcc5651cbc
lriki <lriki.net@gmail.com>
2016-05-06 00:11:15 +0900

    GpuParticle

---
+++ include/Lumino/Scene/GpuParticle.h
@@ -0,0 +1,22 @@
+
+#pragma once
+
+LN_NAMESPACE_BEGIN
+LN_NAMESPACE_SCENE_BEGIN
+
+/**
+	@brief
+*/
+class GpuParticle
+	: public VisualNode
+{
+public:
+
+protected:
+	GpuParticle();
+	virtual ~GpuParticle();
+	void Initialize(SceneGraphManager* manager);
+};
+
+LN_NAMESPACE_SCENE_END
+LN_NAMESPACE_END
