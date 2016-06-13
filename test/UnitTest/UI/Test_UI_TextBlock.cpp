9b1db724a861eb473b8d178a8cc16ba1418c079b
lriki <lriki.net@gmail.com>
2016-06-13 19:05:52 +0900

    ui test

--- test/UnitTest/UI/Test_UI_TextBlock.cpp
+++ test/UnitTest/UI/Test_UI_TextBlock.cpp
@@ -13,14 +13,11 @@ TEST_F(Test_UI_TextBlock, Basic)
 	auto uiRoot = UIContext::GetMainContext()->GetMainWindowView()->GetLayoutRoot();
 	auto textBlock1 = UITextBlock::Create();
 	textBlock1->SetText(_T("TextBlock"));
-	//uiRoot->AddChild(textBlock1);
 	uiRoot->SetContent(textBlock1);
 
 	Engine::UpdateFrame();
 
-
-
-	//ASSERT_TRUE(TestEnv::EqualsScreenShot(LN_LOCALFILE("TestData/IntegrateTest_Scene_Sprite.BlendMode.png")));
-	TestEnv::SaveScreenShot(LN_LOCALFILE("test.png"));
+	ASSERT_TRUE(TestEnv::EqualsScreenShot(LN_LOCALFILE("TestData/Test_UI_TextBlock.Basic.png")));
+	//TestEnv::SaveScreenShot(LN_LOCALFILE("TestData/Test_UI_TextBlock.Basic.png"));
 }
 
