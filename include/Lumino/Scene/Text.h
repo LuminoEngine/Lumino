d3e60d520d44f1f465cfda3b1705d53a8135f97d
lriki <lriki.net@gmail.com>
2016-10-12 00:12:09 +0900

    text

---
+++ include/Lumino/Scene/Text.h
@@ -0,0 +1,44 @@
+﻿
+#pragma once
+#include <Lumino/Graphics/Texture.h>
+#include "VisualNode.h"
+
+LN_NAMESPACE_BEGIN
+LN_NAMESPACE_SCENE_BEGIN
+
+/**
+	@brief
+*/
+class Text2D
+	: public VisualNode
+{
+	LN_TR_REFLECTION_TYPEINFO_DECLARE();
+public:
+	//
+	///**
+	//	@brief		テクスチャを持たないスプライトを作成します。テクスチャを割り当てるには SetTexture() を使用します。
+	//*/
+	//static SpriteBase* Create();
+
+	///**
+	//	@brief		
+	//*/
+	//static SpriteBase* Create3D();
+
+public:
+
+
+protected:
+	Text2D();
+	virtual ~Text2D();
+	void Initialize(SceneGraph* owner, SpriteCoord spriteCoord);
+
+	virtual detail::Sphere GetBoundingSphere() override;
+	virtual void OnRender(SceneGraphRenderingContext* dc) override;
+
+protected:
+	SizeF		m_renderSize;
+};
+
+LN_NAMESPACE_SCENE_END
+LN_NAMESPACE_END
---
+++ src/Scene/Text.cpp
@@ -0,0 +1,37 @@
+﻿
+#include "../Internal.h"
+#include <Lumino/Graphics/GraphicsContext.h>
+#include "SceneGraphManager.h"
+#include "RenderingPass.h"
+#include <Lumino/Scene/SceneGraphRenderingContext.h>
+#include <Lumino/Scene/SceneGraph.h>
+#include <Lumino/Scene/Text.h>
+
+LN_NAMESPACE_BEGIN
+LN_NAMESPACE_SCENE_BEGIN
+
+//==============================================================================
+// Text2D
+//==============================================================================
+LN_TR_REFLECTION_TYPEINFO_IMPLEMENT(Text2D, VisualNode);
+
+//------------------------------------------------------------------------------
+Text2D::Text2D()
+	: VisualNode()
+{
+}
+
+//------------------------------------------------------------------------------
+Text2D::~Text2D()
+{
+}
+
+//------------------------------------------------------------------------------
+void Text2D::Initialize(SceneGraph* owner, SpriteCoord spriteCoord)
+{
+	VisualNode::Initialize(owner, 1);
+}
+
+
+LN_NAMESPACE_SCENE_END
+LN_NAMESPACE_END
