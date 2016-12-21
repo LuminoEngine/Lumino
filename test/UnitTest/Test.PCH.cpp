08d88eeff3a2e81e99ee4fb8933084be94527d0f
lriki <lriki.net@gmail.com>
2016-12-21 12:50:03 +0900

    add pch to test

---
+++ test/UnitTest/Test.PCH.cpp
@@ -0,0 +1,1 @@
+#include "Test.PCH.h"
---
+++ test/UnitTest/Test.PCH.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define LN_INTERNAL_ACCESS				public
+#define LN_PROTECTED_INTERNAL_ACCESS	public
+
+#include <gtest/gtest.h>
+#include <LuminoCore.h>
+#include <LuminoEngine.h>
+#include <Lumino/Testing/TestHelper.h>
