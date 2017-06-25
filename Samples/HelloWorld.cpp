3270652aa8f5b7fa75ebb1d559d83e2abaed99dc
lriki <lriki.net@gmail.com>
2017-06-25 10:13:06 +0900

    tmp

--- Samples/HelloWorld.cpp
+++ Samples/HelloWorld.cpp
@@ -13,7 +13,7 @@ void Main_HelloWorld()
 	while (Engine::update())
 	{
 		printf("---- %d %d\n", ln::detail::g_g_calls, ln::g_g_calls_drawBoxBackground);
-		Engine::getMainWindow()->GetRenderDiagnostic()->print();
+		//Engine::getMainWindow()->GetRenderDiagnostic()->print();
 		ln::detail::g_g_calls = 0;
 		ln::g_g_calls_drawBoxBackground = 0;
 	}
