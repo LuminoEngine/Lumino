b0a039d70ab17fb7b68afc498ae635825c8300cd
lriki <lriki.net@gmail.com>
2017-07-27 17:43:42 +0900

    Improve windows min max macro for MFC

--- Source/LuminoCore/Include/LuminoMath.h
+++ Source/LuminoCore/Include/LuminoMath.h
@@ -2,6 +2,11 @@
 #ifndef LUMINO_MATH_H
 #define LUMINO_MATH_H
 
+#pragma push_macro("min")
+#pragma push_macro("max")
+#undef min
+#undef max
+
 #include "Lumino/Math/MathUtils.h"
 #include "Lumino/Math/Vector2.h"
 #include "Lumino/Math/Vector3.h"
@@ -13,5 +18,8 @@
 #include "Lumino/Math/Plane.h"
 #include "Lumino/Math/ViewFrustum.h"
 #include "Lumino/Math/Random.h"
+
+#pragma pop_macro("min")
+#pragma pop_macro("max")
 
 #endif // LUMINO_MATH_H
