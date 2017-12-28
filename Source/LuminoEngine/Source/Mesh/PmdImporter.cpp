7078b01bd3246daa0b4f72aaa0df55cb2fc1e1f3
lriki <lriki.net@gmail.com>
2017-12-29 00:18:26 +0900

    add PmdImporter

---
+++ Source/LuminoEngine/Source/Mesh/PmdImporter.cpp
@@ -0,0 +1,23 @@
+﻿
+#include "../Internal.h"
+#include <Lumino/IO/BinaryReader.h>
+#include "ModelManager.h"
+#include "PmxLoader.h"
+
+LN_NAMESPACE_BEGIN
+
+
+//==============================================================================
+// PmdImporter
+//==============================================================================
+
+PmdImporter::PmdImporter()
+{
+}
+
+PmdImporter::~PmdImporter()
+{
+}
+
+
+LN_NAMESPACE_END
---
+++ Source/LuminoEngine/Source/Mesh/PmdImporter.h
@@ -0,0 +1,18 @@
+﻿
+#pragma once
+#include "Common.h"
+
+LN_NAMESPACE_BEGIN
+class BinaryReader;
+class Texture2D;
+namespace detail { class ModelManager; }
+
+class PmdImporter
+{
+public:
+	PmdImporter();
+	virtual ~PmdImporter();
+};
+
+LN_NAMESPACE_END
+
