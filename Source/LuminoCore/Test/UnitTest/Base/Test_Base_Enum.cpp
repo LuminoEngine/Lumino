19a41e9f35a50a423322c5838337bdcaa75d014c
lriki <lriki.net@gmail.com>
2018-01-16 12:52:12 +0900

    add Flags(enum) test

--- Source/LuminoCore/Test/UnitTest/Base/Test_Base_Enum.cpp
+++ Source/LuminoCore/Test/UnitTest/Base/Test_Base_Enum.cpp
@@ -1,5 +1,337 @@
 ﻿#include <TestConfig.h>
 
+
+namespace ln
+{
+typedef uint32_t FlagsType;
+
+template <typename FlagBitsType> struct FlagTraits
+{
+	enum { allFlags = 0 };
+};
+
+template<typename EnumType, typename MaskType = FlagsType>
+class Flags
+{
+public:
+	Flags()
+		: m_mask(0)
+	{
+	}
+
+	Flags(EnumType bit)
+		: m_mask(static_cast<MaskType>(bit))
+	{
+	}
+
+	Flags(Flags<EnumType> const& rhs)
+		: m_mask(rhs.m_mask)
+	{
+	}
+
+	explicit Flags(MaskType flags)
+		: m_mask(flags)
+	{
+	}
+
+	bool isSet(EnumType value) const
+	{
+		return (m_mask & static_cast<MaskType>(value)) == static_cast<MaskType>(value);
+	}
+
+	Flags<EnumType>& set(EnumType value)
+	{
+		m_mask |= static_cast<MaskType>(value);
+		return *this;
+	}
+
+	void unset(EnumType value)
+	{
+		m_mask &= ~static_cast<MaskType>(value);
+	}
+
+	Flags<EnumType> & operator=(Flags<EnumType> const& rhs)
+	{
+		m_mask = rhs.m_mask;
+		return *this;
+	}
+
+	Flags<EnumType> & operator|=(Flags<EnumType> const& rhs)
+	{
+		m_mask |= rhs.m_mask;
+		return *this;
+	}
+
+	Flags<EnumType> & operator&=(Flags<EnumType> const& rhs)
+	{
+		m_mask &= rhs.m_mask;
+		return *this;
+	}
+
+	Flags<EnumType> & operator^=(Flags<EnumType> const& rhs)
+	{
+		m_mask ^= rhs.m_mask;
+		return *this;
+	}
+
+	Flags<EnumType> operator|(Flags<EnumType> const& rhs) const
+	{
+		Flags<EnumType> result(*this);
+		result |= rhs;
+		return result;
+	}
+
+	Flags<EnumType> operator&(Flags<EnumType> const& rhs) const
+	{
+		Flags<EnumType> result(*this);
+		result &= rhs;
+		return result;
+	}
+
+	Flags<EnumType> operator^(Flags<EnumType> const& rhs) const
+	{
+		Flags<EnumType> result(*this);
+		result ^= rhs;
+		return result;
+	}
+
+	bool operator!() const
+	{
+		return !m_mask;
+	}
+
+	Flags<EnumType> operator~() const
+	{
+		Flags<EnumType> result(*this);
+		result.m_mask = (MaskType)~m_mask;//^= FlagTraits<EnumType>::allFlags;
+		return result;
+	}
+
+	bool operator==(Flags<EnumType> const& rhs) const
+	{
+		return m_mask == rhs.m_mask;
+	}
+
+	bool operator!=(Flags<EnumType> const& rhs) const
+	{
+		return m_mask != rhs.m_mask;
+	}
+
+	explicit operator bool() const
+	{
+		return !!m_mask;
+	}
+
+	explicit operator MaskType() const
+	{
+		return m_mask;
+	}
+
+private:
+	MaskType  m_mask;
+};
+
+//template <typename EnumType>
+//Flags<EnumType> operator|(EnumType bit, Flags<EnumType> const& flags)
+//{
+//	return flags | bit;
+//}
+//
+//template <typename EnumType>
+//Flags<EnumType> operator&(EnumType bit, Flags<EnumType> const& flags)
+//{
+//	return flags & bit;
+//}
+//
+//template <typename EnumType>
+//Flags<EnumType> operator^(EnumType bit, Flags<EnumType> const& flags)
+//{
+//	return flags ^ bit;
+//}
+
+#define LN_FLAGS_OPERATORS(EnumType) \
+	inline bool operator==(EnumType lhs, const ln::Flags<EnumType>& rhs) { return rhs == lhs; } \
+	inline bool operator!=(EnumType lhs, const ln::Flags<EnumType>& rhs) { return rhs != lhs; } \
+	inline ln::Flags<EnumType> operator|(EnumType lhs, const ln::Flags<EnumType>& rhs) { return rhs | lhs; } \
+	inline ln::Flags<EnumType> operator&(EnumType lhs, const ln::Flags<EnumType>& rhs) { return rhs & lhs; } \
+	inline ln::Flags<EnumType> operator^(EnumType lhs, const ln::Flags<EnumType>& rhs) { return rhs ^ lhs; } \
+	inline ln::Flags<EnumType> operator|(EnumType lhs, EnumType rhs) { return ln::Flags<EnumType>(lhs) | rhs; } \
+	inline ln::Flags<EnumType> operator&(EnumType lhs, EnumType rhs) { return ln::Flags<EnumType>(lhs) & rhs; } \
+	inline ln::Flags<EnumType> operator^(EnumType lhs, EnumType rhs) { return ln::Flags<EnumType>(lhs) ^ rhs; } \
+	inline ln::Flags<EnumType> operator~(EnumType a) { return ~ln::Flags<EnumType>(a); }
+
+
+} // namespace ln
+
+
+//==============================================================================
+//# enum class のビットフラグのテスト
+class Test_Base_EnumClassBitFlags : public ::testing::Test
+{
+protected:
+	virtual void SetUp() {}
+	virtual void TearDown() {}
+};
+
+enum class TestEnum1 : ln::FlagsType
+{
+	V0 = 0x0000,
+	V1 = 0x0001,
+	V2 = 0x0002,
+	V3 = 0x0004,
+	V1V2 = V1 | V2,
+	All = V1 | V2 | V3,
+};
+LN_FLAGS_OPERATORS(TestEnum1);
+
+//------------------------------------------------------------------------------
+TEST_F(Test_Base_EnumClassBitFlags, Basic)
+{
+	Flags<TestEnum1> flags1;
+	flags1 = TestEnum1::V1 | TestEnum1::V2;
+	ASSERT_EQ(TestEnum1::V1 | TestEnum1::V2, flags1);
+
+	flags1 |= TestEnum1::V3;
+	ASSERT_EQ(TestEnum1::All, flags1);
+
+	//flags1 = 0;                // -> NG
+}
+
+//------------------------------------------------------------------------------
+//## operator のテスト
+TEST_F(Test_Base_EnumClassBitFlags, Operators)
+{
+	Flags<TestEnum1> flags1;
+
+	//-[ ] 代入系
+	{
+		flags1 = TestEnum1::V1;
+		flags1 &= TestEnum1::V2;
+		ASSERT_EQ(TestEnum1::V0, flags1);
+
+		flags1 = TestEnum1::V1;
+		flags1 |= TestEnum1::V2;
+		ASSERT_EQ(TestEnum1::V1V2, flags1);
+
+		flags1 = TestEnum1::V1;
+		flags1 ^= TestEnum1::V1V2;
+		ASSERT_EQ(TestEnum1::V2, flags1);
+	}
+
+	//-[ ] 左辺 Flags<> 系
+	{
+		flags1 = TestEnum1::V1;
+		ASSERT_EQ(true, flags1 == TestEnum1::V1);
+		ASSERT_EQ(false, flags1 != TestEnum1::V1);
+
+		flags1 = Flags<TestEnum1>(TestEnum1::V1) & TestEnum1::V1;
+		ASSERT_EQ(TestEnum1::V1, flags1);
+
+		flags1 = Flags<TestEnum1>(TestEnum1::V1) |TestEnum1::V2;
+		ASSERT_EQ(TestEnum1::V1V2, flags1);
+
+		flags1 = Flags<TestEnum1>(TestEnum1::V1) ^ TestEnum1::V1V2;
+		ASSERT_EQ(TestEnum1::V2, flags1);
+
+		flags1 = ~Flags<TestEnum1>(TestEnum1::V1);
+		ASSERT_EQ(0xfffffffe, static_cast<uint32_t>(flags1));
+	}
+
+	//-[ ] 左辺 enum class 系
+	{
+		flags1 = TestEnum1::V1;
+		ASSERT_EQ(true, TestEnum1::V1 == Flags<TestEnum1>(TestEnum1::V1));
+		ASSERT_EQ(false, TestEnum1::V1 != Flags<TestEnum1>(TestEnum1::V1));
+
+		flags1 = TestEnum1::V1 & Flags<TestEnum1>(TestEnum1::V1);
+		ASSERT_EQ(TestEnum1::V1, flags1);
+
+		flags1 = TestEnum1::V1 | Flags<TestEnum1>(TestEnum1::V2);
+		ASSERT_EQ(TestEnum1::V1V2, flags1);
+
+		flags1 = TestEnum1::V1 ^ Flags<TestEnum1>(TestEnum1::V1V2);
+		ASSERT_EQ(TestEnum1::V2, flags1);
+	}
+
+	//-[ ] 両辺 enum class 系
+	{
+		flags1 = TestEnum1::V1 & TestEnum1::V1;
+		ASSERT_EQ(TestEnum1::V1, flags1);
+
+		flags1 = TestEnum1::V1 | TestEnum1::V2;
+		ASSERT_EQ(TestEnum1::V1V2, flags1);
+
+		flags1 = TestEnum1::V1 ^ TestEnum1::V1V2;
+		ASSERT_EQ(TestEnum1::V2, flags1);
+
+		flags1 = ~TestEnum1::V1;
+		ASSERT_EQ(0xfffffffe, (uint32_t)flags1);
+	}
+}
+
+enum class TestEnum2 : ln::FlagsType
+{
+	V0 = 0x0000,
+	V1 = 0x0001,
+	V2 = 0x0002,
+};
+LN_FLAGS_OPERATORS(TestEnum2);
+
+//------------------------------------------------------------------------------
+//## メソッドによるフラグ操作のテスト
+TEST_F(Test_Base_EnumClassBitFlags, SimpleAccessors)
+{
+	Flags<TestEnum2> flags1;
+
+	//-[ ] isSet
+	{
+		bool b1 = (bool)flags1;
+		ASSERT_EQ(false, b1);
+
+		ASSERT_EQ(false, flags1.isSet(TestEnum2::V1));
+		ASSERT_EQ(false, flags1.isSet(TestEnum2::V2));
+	}
+
+	//-[ ] set
+	{
+		flags1.set(TestEnum2::V1);
+		ASSERT_EQ(true, flags1.isSet(TestEnum2::V1));
+		ASSERT_EQ(false, flags1.isSet(TestEnum2::V2));
+
+		flags1.set(TestEnum2::V2);
+		ASSERT_EQ(true, flags1.isSet(TestEnum2::V1));
+		ASSERT_EQ(true, flags1.isSet(TestEnum2::V2));
+
+		bool b2 = static_cast<bool>(flags1);
+		ASSERT_EQ(true, b2);
+	}
+
+	//-[ ] unset
+	{
+		flags1.unset(TestEnum2::V1);
+		ASSERT_EQ(false, flags1.isSet(TestEnum2::V1));
+		ASSERT_EQ(true, flags1.isSet(TestEnum2::V2));
+
+		flags1.unset(TestEnum2::V2);
+		ASSERT_EQ(false, flags1.isSet(TestEnum2::V1));
+		ASSERT_EQ(false, flags1.isSet(TestEnum2::V2));
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Test_Base_Enum : public ::testing::Test
 {
 protected:
