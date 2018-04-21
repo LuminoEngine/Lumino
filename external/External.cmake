4214dff662d1258a26e8e628b18065ac3c1a7dcb
lriki <lriki.net@gmail.com>
2018-04-22 01:58:15 +0900

    add glfw

--- external/External.cmake
+++ external/External.cmake
@@ -8,3 +8,8 @@ set(LN_INCLUDES_gtest
 set(LN_SOURCES_gtest
 	"${CMAKE_CURRENT_LIST_DIR}/gtest/src/gtest-all.cc")
 
+set(GLFW_BUILD_EXAMPLES OFF)
+set(GLFW_BUILD_TESTS OFF)
+set(GLFW_BUILD_DOCS OFF)
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/glfw-3.2.1)
+
