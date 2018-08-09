2fbf6d1f0e381ac94146ae222d948f2789fdb2e4
lriki <lriki.net@gmail.com>
2018-08-09 23:07:35 +0900

    fix glad build

--- tools/LuminoBuild/Main.cs
+++ tools/LuminoBuild/Main.cs
@@ -12,7 +12,7 @@ namespace LuminoBuild
             // default
             if (args.Length == 0)
             {
-                args = new string[] { "BuildEngineEmscripten" };
+                args = new string[] { "MakeVSProjects" };
             }
 
             Assembly thisAssembly = Assembly.GetEntryAssembly();
