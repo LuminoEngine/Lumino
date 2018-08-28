fd4c05fa652b440b9a3fdc616fc51d5424ff09af
lriki <lriki.net@gmail.com>
2018-08-28 22:43:37 +0900

    installer

---
+++ tools/LuminoBuild/Tasks/DeployEnginePackage.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LuminoBuild.Tasks
+{
+    class DeployEnginePackage : BuildTask
+    {
+        public override string CommandName => "DeployEnginePackage";
+
+        public override string Description => "DeployEnginePackage";
+
+        public override void Build(Builder builder)
+        {
+            var tempInstallDir = Path.Combine(builder.LuminoBuildDir, BuildEnvironment.CMakeTargetInstallDir);
+            var targetRootDir = Path.Combine(builder.LuminoBuildDir, "ReleasePackage");
+
+            // C++ Engine
+            {
+                string cppEngineRoot = Path.Combine(targetRootDir, "Engine", "Cpp");
+
+                Utils.CopyDirectory(
+                    Path.Combine(builder.LuminoRootDir, "src", "LuminoCore", "include"),
+                    Path.Combine(cppEngineRoot, "include"));
+
+                Utils.CopyDirectory(
+                    Path.Combine(builder.LuminoRootDir, "src", "LuminoEngine", "include"),
+                    Path.Combine(cppEngineRoot, "include"));
+
+
+                var engineArchs = new string[]
+                {
+                    "MSVC2017-x86-MD",
+                    "MSVC2017-x86-MT",
+                    "MSVC2017-x64-MD",
+                    "Emscripten",
+                };
+
+                var externalLibs = new string[]
+                {
+                    "glad",
+                    "glslang",
+                    "libpng",
+                    "openal-soft",
+                    "SDL2",
+                    "SPIRV-Cross",
+                    "zlib",
+                };
+
+                var externalInstallDir = Path.Combine(builder.LuminoBuildDir, "MSVC2017-x86-MD", "ExternalInstall");
+                
+                foreach (var arch in engineArchs)
+                {
+                    Utils.CopyDirectory(
+                        Path.Combine(tempInstallDir, arch, "lib"),
+                        Path.Combine(cppEngineRoot, "lib", arch));
+
+                    foreach (var lib in externalLibs)
+                    {
+                        Utils.CopyDirectory(
+                            Path.Combine(externalInstallDir, lib, "lib"),
+                            Path.Combine(cppEngineRoot, "lib", "MSVC2017-x86-MD"));
+                    }
+                }
+            }
+        }
+    }
+}
