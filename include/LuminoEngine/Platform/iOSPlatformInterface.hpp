9a61ec755ca32dcfbb912007498617cd909a4971
lriki <lriki.net@gmail.com>
2018-10-14 21:32:10 +0900

    ios

---
+++ include/LuminoEngine/Platform/iOSPlatformInterface.hpp
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+namespace ln {
+
+class iOSPlatformInterface
+{
+public:
+
+	static void nativeInitialize(int viewWidth, int viewHeight);
+
+	static void nativeUpdateFrame();
+
+	static void nativeFinalize();
+
+	static void applicationDidEnterBackground();
+
+	static void applicationWillEnterForeground();
+
+	static void applicationScreenSizeChanged(int viewWidth, int viewHeight);
+};
+
+} // namespace ln
