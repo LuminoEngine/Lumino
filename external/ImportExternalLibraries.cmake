de21c27030bef9a7a866569b152c327b5a4e243d
lriki <lriki.net@gmail.com>
2018-10-18 22:46:17 +0900

    gtest

--- external/ImportExternalLibraries.cmake
+++ external/ImportExternalLibraries.cmake
@@ -29,14 +29,6 @@ if (MSVC)
 endif()
 
 
-set(LN_INCLUDES_gtest
-    "${CMAKE_CURRENT_LIST_DIR}/gtest/include"
-    "${CMAKE_CURRENT_LIST_DIR}/gtest"
-    "${CMAKE_CURRENT_LIST_DIR}/gtest/src")
-
-set(LN_SOURCES_gtest
-    "${CMAKE_CURRENT_LIST_DIR}/gtest/src/gtest-all.cc")
-
 #-------------------------------------------------------------------------------
 
 message("EMSCRIPTEN_ROOT_PATH: ${EMSCRIPTEN_ROOT_PATH}")
