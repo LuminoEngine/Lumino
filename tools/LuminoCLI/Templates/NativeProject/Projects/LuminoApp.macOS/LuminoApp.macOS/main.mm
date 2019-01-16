579ce119d70ab5821bc56f69f897c815c39ca160
lriki <lriki.net@gmail.com>
2019-01-16 21:52:14 +0900

    macos

--- tools/LuminoCLI/Templates/NativeProject/Projects/LuminoApp.macOS/LuminoApp.macOS/main.mm
+++ tools/LuminoCLI/Templates/NativeProject/Projects/LuminoApp.macOS/LuminoApp.macOS/main.mm
@@ -7,15 +7,15 @@ int main(int argc, const char * argv[])
 {
     ln::GlobalLogger::addStdErrAdapter();
 
-#ifdef LN_DEBUG
-    auto projectRoot = ln::Path(ln::Environment::executablePath()).parent().parent().parent().parent().parent();
+#if defined(LN_DEBUG) && defined(LN_XCODE_PROJECT_DIR)
+	auto projectRoot = ln::Path(ln::String::fromCString(LN_XCODE_PROJECT_DIR)).parent().parent();
     ln::EngineSettings::addAssetDirectory(ln::Path::combine(projectRoot, u"Assets"));
     ln::EngineSettings::addAssetDirectory(ln::Path::combine(projectRoot, u".ln", u"Assets"));
 #endif
 
     NSBundle* bundle = [NSBundle mainBundle];
     NSString* assetsPath = [bundle pathForResource:@"Assets" ofType:@"lca"];
-    ln::Engine::addAssetArchive(
+    ln::EngineSettings::addAssetArchive(
         ln::String::fromStdString(std::string([assetsPath UTF8String])));
 
     return ln::CocoaPlatformInterface::Main();
