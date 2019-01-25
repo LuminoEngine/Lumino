7b3372a7df53054202a2deced53873fb2afadc41
lriki <lriki.net@gmail.com>
2019-01-26 01:28:04 +0900

    add VulkanDeviceContext

---
+++ src/LuminoEngine/src/Graphics/VulkanDeviceContext.cpp
@@ -0,0 +1,696 @@
+﻿
+#include "Internal.hpp"
+#include <LuminoEngine/Platform/PlatformWindow.hpp>
+#include "VulkanDeviceContext.hpp"
+
+namespace ln {
+namespace detail {
+
+//=============================================================================
+// VulkanDeviceContext
+
+VulkanDeviceContext::VulkanDeviceContext()
+{
+}
+
+VulkanDeviceContext::~VulkanDeviceContext()
+{
+}
+
+void VulkanDeviceContext::init(const Settings& settings)
+{
+}
+
+void VulkanDeviceContext::dispose()
+{
+    IGraphicsDeviceContext::dispose();
+}
+
+void VulkanDeviceContext::onGetCaps(GraphicsDeviceCaps* outCaps)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanDeviceContext::onEnterMainThread()
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanDeviceContext::onLeaveMainThread()
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanDeviceContext::onSaveExternalRenderState()
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanDeviceContext::onRestoreExternalRenderState()
+{
+    LN_NOTIMPLEMENTED();
+}
+
+Ref<ISwapChain> VulkanDeviceContext::onCreateSwapChain(PlatformWindow* window, const SizeI& backbufferSize)
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+Ref<IVertexDeclaration> VulkanDeviceContext::onCreateVertexDeclaration(const VertexElement* elements, int elementsCount)
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+Ref<IVertexBuffer> VulkanDeviceContext::onCreateVertexBuffer(GraphicsResourceUsage usage, size_t bufferSize, const void* initialData)
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+Ref<IIndexBuffer> VulkanDeviceContext::onCreateIndexBuffer(GraphicsResourceUsage usage, IndexBufferFormat format, int indexCount, const void* initialData)
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+Ref<ITexture> VulkanDeviceContext::onCreateTexture2D(uint32_t width, uint32_t height, TextureFormat requestFormat, bool mipmap, const void* initialData)
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+Ref<ITexture> VulkanDeviceContext::onCreateTexture3D(uint32_t width, uint32_t height, uint32_t depth, TextureFormat requestFormat, bool mipmap, const void* initialData)
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+Ref<ITexture> VulkanDeviceContext::onCreateRenderTarget(uint32_t width, uint32_t height, TextureFormat requestFormat, bool mipmap)
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+Ref<IDepthBuffer> VulkanDeviceContext::onCreateDepthBuffer(uint32_t width, uint32_t height)
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+Ref<ISamplerState> VulkanDeviceContext::onCreateSamplerState(const SamplerStateData& desc)
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+Ref<IShaderPass> VulkanDeviceContext::onCreateShaderPass(const byte_t* vsCode, int vsCodeLen, const byte_t* psCode, int psCodeLen, ShaderCompilationDiag* diag)
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+void VulkanDeviceContext::onUpdatePipelineState(const BlendStateDesc& blendState, const RasterizerStateDesc& rasterizerState, const DepthStencilStateDesc& depthStencilState)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanDeviceContext::onUpdateFrameBuffers(ITexture** renderTargets, int renderTargetsCount, IDepthBuffer* depthBuffer)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanDeviceContext::onUpdateRegionRects(const RectI& viewportRect, const RectI& scissorRect, const SizeI& targetSize)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanDeviceContext::onUpdatePrimitiveData(IVertexDeclaration* decls, IVertexBuffer** vertexBuufers, int vertexBuffersCount, IIndexBuffer* indexBuffer)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanDeviceContext::onUpdateShaderPass(IShaderPass* newPass)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanDeviceContext::onClearBuffers(ClearFlags flags, const Color& color, float z, uint8_t stencil)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanDeviceContext::onDrawPrimitive(PrimitiveType primitive, int startVertex, int primitiveCount)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanDeviceContext::onDrawPrimitiveIndexed(PrimitiveType primitive, int startIndex, int primitiveCount)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanDeviceContext::onPresent(ISwapChain* swapChain)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+//=============================================================================
+// VulkanSwapChain
+
+VulkanSwapChain::VulkanSwapChain()
+{
+}
+
+VulkanSwapChain::~VulkanSwapChain()
+{
+}
+
+void VulkanSwapChain::dispose()
+{
+    ISwapChain::dispose();
+}
+
+ITexture* VulkanSwapChain::getColorBuffer() const
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+
+//==============================================================================
+// VulkanVertexDeclaration
+
+VulkanVertexDeclaration::VulkanVertexDeclaration()
+{
+}
+
+VulkanVertexDeclaration::~VulkanVertexDeclaration()
+{
+}
+
+void VulkanVertexDeclaration::init(const VertexElement* elements, int elementsCount)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanVertexDeclaration::dispose()
+{
+    IVertexDeclaration::dispose();
+}
+
+//==============================================================================
+// VulkanVertexBuffer
+
+VulkanVertexBuffer::VulkanVertexBuffer()
+{
+}
+
+VulkanVertexBuffer::~VulkanVertexBuffer()
+{
+}
+
+void VulkanVertexBuffer::init(GraphicsResourceUsage usage, size_t bufferSize, const void* initialData)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanVertexBuffer::dispose()
+{
+    IVertexBuffer::dispose();
+}
+
+size_t VulkanVertexBuffer::getBytesSize()
+{
+    LN_NOTIMPLEMENTED();
+    return 0;
+}
+
+GraphicsResourceUsage VulkanVertexBuffer::usage() const
+{
+    LN_NOTIMPLEMENTED();
+    return GraphicsResourceUsage::Static;
+}
+
+void VulkanVertexBuffer::setSubData(size_t offset, const void* data, size_t length)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void* VulkanVertexBuffer::map()
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+void VulkanVertexBuffer::unmap()
+{
+    LN_NOTIMPLEMENTED();
+}
+
+//==============================================================================
+// VulkanIndexBuffer
+
+VulkanIndexBuffer::VulkanIndexBuffer()
+{
+}
+
+VulkanIndexBuffer::~VulkanIndexBuffer()
+{
+}
+
+void VulkanIndexBuffer::init(GraphicsResourceUsage usage, IndexBufferFormat format, int indexCount, const void* initialData)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanIndexBuffer::dispose()
+{
+    LN_NOTIMPLEMENTED();
+    IIndexBuffer::dispose();
+}
+
+size_t VulkanIndexBuffer::getBytesSize()
+{
+    LN_NOTIMPLEMENTED();
+    return 0;
+}
+
+GraphicsResourceUsage VulkanIndexBuffer::usage() const
+{
+    LN_NOTIMPLEMENTED();
+    return GraphicsResourceUsage::Static;
+}
+
+void VulkanIndexBuffer::setSubData(size_t offset, const void* data, size_t length)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void* VulkanIndexBuffer::map()
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+void VulkanIndexBuffer::unmap()
+{
+    LN_NOTIMPLEMENTED();
+}
+
+//=============================================================================
+// VulkanTextureBase
+
+VulkanTextureBase::VulkanTextureBase()
+{
+}
+
+VulkanTextureBase::~VulkanTextureBase()
+{
+}
+
+//=============================================================================
+// VulkanTexture2D
+
+VulkanTexture2D::VulkanTexture2D()
+{
+}
+
+VulkanTexture2D::~VulkanTexture2D()
+{
+}
+
+void VulkanTexture2D::init(uint32_t width, uint32_t height, TextureFormat requestFormat, bool mipmap, const void* initialData)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanTexture2D::dispose()
+{
+    LN_NOTIMPLEMENTED();
+    VulkanTextureBase::dispose();
+}
+
+DeviceTextureType VulkanTexture2D::type() const
+{
+    return DeviceTextureType::Texture2D;
+}
+
+void VulkanTexture2D::readData(void* outData)
+{
+    LN_UNREACHABLE();
+}
+
+const SizeI& VulkanTexture2D::realSize()
+{
+    LN_NOTIMPLEMENTED();
+    return SizeI::Zero;
+}
+
+TextureFormat VulkanTexture2D::getTextureFormat() const
+{
+    LN_NOTIMPLEMENTED();
+    return TextureFormat::Unknown;
+}
+
+void VulkanTexture2D::setSubData(int x, int y, int width, int height, const void* data, size_t dataSize)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanTexture2D::setSubData3D(int x, int y, int z, int width, int height, int depth, const void* data, size_t dataSize)
+{
+    LN_UNREACHABLE();
+}
+
+//=============================================================================
+// VulkanTexture3D
+
+VulkanTexture3D::VulkanTexture3D()
+{
+}
+
+VulkanTexture3D::~VulkanTexture3D()
+{
+}
+
+void VulkanTexture3D::init(uint32_t width, uint32_t height, uint32_t depth, TextureFormat requestFormat, bool mipmap, const void* initialData)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanTexture3D::dispose()
+{
+    LN_NOTIMPLEMENTED();
+    VulkanTextureBase::dispose();
+}
+
+DeviceTextureType VulkanTexture3D::type() const
+{
+    return DeviceTextureType::Texture3D;
+}
+
+void VulkanTexture3D::readData(void* outData)
+{
+    LN_UNREACHABLE();
+}
+
+const SizeI& VulkanTexture3D::realSize()
+{
+    LN_NOTIMPLEMENTED();
+    return SizeI::Zero;
+}
+
+TextureFormat VulkanTexture3D::getTextureFormat() const
+{
+    LN_NOTIMPLEMENTED();
+    return TextureFormat::Unknown;
+}
+
+void VulkanTexture3D::setSubData(int x, int y, int width, int height, const void* data, size_t dataSize)
+{
+    LN_UNREACHABLE();
+}
+
+void VulkanTexture3D::setSubData3D(int x, int y, int z, int width, int height, int depth, const void* data, size_t dataSize)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+//=============================================================================
+// VulkanRenderTargetTexture
+
+VulkanRenderTargetTexture::VulkanRenderTargetTexture()
+{
+}
+
+VulkanRenderTargetTexture::~VulkanRenderTargetTexture()
+{
+}
+
+void VulkanRenderTargetTexture::init(uint32_t width, uint32_t height, TextureFormat requestFormat, bool mipmap)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanRenderTargetTexture::dispose()
+{
+    LN_NOTIMPLEMENTED();
+    VulkanTextureBase::dispose();
+}
+
+DeviceTextureType VulkanRenderTargetTexture::type() const
+{
+    return DeviceTextureType::RenderTarget;
+}
+
+void VulkanRenderTargetTexture::readData(void* outData)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+const SizeI& VulkanRenderTargetTexture::realSize()
+{
+    LN_NOTIMPLEMENTED();
+    return SizeI::Zero;
+}
+
+TextureFormat VulkanRenderTargetTexture::getTextureFormat() const
+{
+    LN_NOTIMPLEMENTED();
+    return TextureFormat::Unknown;
+}
+
+void VulkanRenderTargetTexture::setSubData(int x, int y, int width, int height, const void* data, size_t dataSize)
+{
+    LN_UNREACHABLE();
+}
+
+void VulkanRenderTargetTexture::setSubData3D(int x, int y, int z, int width, int height, int depth, const void* data, size_t dataSize)
+{
+    LN_UNREACHABLE();
+}
+
+//=============================================================================
+// VulkanDepthBuffer
+
+VulkanDepthBuffer::VulkanDepthBuffer()
+{
+}
+
+VulkanDepthBuffer::~VulkanDepthBuffer()
+{
+}
+
+void VulkanDepthBuffer::init(uint32_t width, uint32_t height)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanDepthBuffer::dispose()
+{
+    IDepthBuffer::dispose();
+}
+
+//=============================================================================
+// VulkanSamplerState
+
+VulkanSamplerState::VulkanSamplerState()
+{
+}
+
+VulkanSamplerState::~VulkanSamplerState()
+{
+}
+
+void VulkanSamplerState::init(const SamplerStateData& desc)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanSamplerState::dispose()
+{
+    LN_NOTIMPLEMENTED();
+    ISamplerState::dispose();
+}
+
+//=============================================================================
+// VulkanShaderPass
+
+VulkanShaderPass::VulkanShaderPass()
+{
+}
+
+VulkanShaderPass::~VulkanShaderPass()
+{
+}
+
+void VulkanShaderPass::init(VulkanDeviceContext* context, const byte_t* vsCode, int vsCodeLen, const byte_t* fsCode, int fsCodeLen, ShaderCompilationDiag* diag)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanShaderPass::dispose()
+{
+    IShaderPass::dispose();
+}
+
+int VulkanShaderPass::getUniformCount() const
+{
+    LN_NOTIMPLEMENTED();
+    return 0;
+}
+
+IShaderUniform* VulkanShaderPass::getUniform(int index) const
+{
+    // TODO: 必要？
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+void VulkanShaderPass::setUniformValue(int index, const void* data, size_t size)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+int VulkanShaderPass::getUniformBufferCount() const
+{
+    LN_NOTIMPLEMENTED();
+    return 0;
+}
+
+IShaderUniformBuffer* VulkanShaderPass::getUniformBuffer(int index) const
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+IShaderSamplerBuffer* VulkanShaderPass::samplerBuffer() const
+{
+    LN_NOTIMPLEMENTED();
+    return 0;
+}
+
+//=============================================================================
+// VulkanShaderUniformBuffer
+
+VulkanShaderUniformBuffer::VulkanShaderUniformBuffer()
+{
+}
+
+VulkanShaderUniformBuffer::~VulkanShaderUniformBuffer()
+{
+}
+
+void VulkanShaderUniformBuffer::init()
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanShaderUniformBuffer::dispose()
+{
+    IShaderUniformBuffer::dispose();
+}
+
+const std::string& VulkanShaderUniformBuffer::name() const
+{
+    LN_NOTIMPLEMENTED();
+    return m_name;
+}
+
+int VulkanShaderUniformBuffer::getUniformCount() const
+{
+    LN_NOTIMPLEMENTED();
+    return 0;
+}
+
+IShaderUniform* VulkanShaderUniformBuffer::getUniform(int index) const
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
+
+size_t VulkanShaderUniformBuffer::bufferSize() const
+{
+    LN_NOTIMPLEMENTED();
+    return 0;
+}
+
+void VulkanShaderUniformBuffer::setData(const void* data, size_t size)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+
+//=============================================================================
+// VulkanShaderUniform
+
+VulkanShaderUniform::VulkanShaderUniform()
+{
+}
+
+VulkanShaderUniform::~VulkanShaderUniform()
+{
+}
+
+void VulkanShaderUniform::init()
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanShaderUniform::dispose()
+{
+    IShaderUniform::dispose();
+}
+
+//=============================================================================
+// VulkanLocalShaderSamplerBuffer
+
+VulkanLocalShaderSamplerBuffer::VulkanLocalShaderSamplerBuffer()
+{
+}
+
+VulkanLocalShaderSamplerBuffer::~VulkanLocalShaderSamplerBuffer()
+{
+}
+
+void VulkanLocalShaderSamplerBuffer::init()
+{
+}
+
+void VulkanLocalShaderSamplerBuffer::dispose()
+{
+}
+
+int VulkanLocalShaderSamplerBuffer::registerCount() const
+{
+    LN_NOTIMPLEMENTED();
+    return 0;
+}
+
+const std::string& VulkanLocalShaderSamplerBuffer::getTextureRegisterName(int registerIndex) const
+{
+    LN_NOTIMPLEMENTED();
+    return std::string();
+}
+
+const std::string& VulkanLocalShaderSamplerBuffer::getSamplerRegisterName(int registerIndex) const
+{
+    LN_NOTIMPLEMENTED();
+    return std::string();
+}
+
+void VulkanLocalShaderSamplerBuffer::setTexture(int registerIndex, ITexture* texture)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+void VulkanLocalShaderSamplerBuffer::setSamplerState(int registerIndex, ISamplerState* state)
+{
+    LN_NOTIMPLEMENTED();
+}
+
+} // namespace detail
+} // namespace ln
+
---
+++ src/LuminoEngine/src/Graphics/VulkanDeviceContext.hpp
@@ -0,0 +1,279 @@
+﻿
+#pragma once
+
+#include <vulkan/vulkan.h>
+#include "GraphicsDeviceContext.hpp"
+
+namespace ln {
+namespace detail {
+class VulkanSwapChain;
+class VulkanIndexBuffer;
+class VulkanRenderTargetTexture;
+class VulkanShaderPass;
+class VulkanShaderUniformBuffer;
+class VulkanShaderUniform;
+class VulkanLocalShaderSamplerBuffer;
+
+class VulkanDeviceContext
+    : public IGraphicsDeviceContext
+{
+public:
+    struct Settings
+    {
+        PlatformWindow* mainWindow = nullptr;
+    };
+
+    VulkanDeviceContext();
+    virtual ~VulkanDeviceContext();
+    void init(const Settings& settings);
+    virtual void dispose() override;
+
+protected:
+    virtual void onGetCaps(GraphicsDeviceCaps* outCaps) override;
+    virtual void onEnterMainThread() override;
+    virtual void onLeaveMainThread() override;
+    virtual void onSaveExternalRenderState() override;
+    virtual void onRestoreExternalRenderState() override;
+    virtual Ref<ISwapChain> onCreateSwapChain(PlatformWindow* window, const SizeI& backbufferSize) override;
+    virtual Ref<IVertexDeclaration> onCreateVertexDeclaration(const VertexElement* elements, int elementsCount) override;
+    virtual Ref<IVertexBuffer> onCreateVertexBuffer(GraphicsResourceUsage usage, size_t bufferSize, const void* initialData) override;
+    virtual Ref<IIndexBuffer> onCreateIndexBuffer(GraphicsResourceUsage usage, IndexBufferFormat format, int indexCount, const void* initialData) override;
+    virtual Ref<ITexture> onCreateTexture2D(uint32_t width, uint32_t height, TextureFormat requestFormat, bool mipmap, const void* initialData) override;
+    virtual Ref<ITexture> onCreateTexture3D(uint32_t width, uint32_t height, uint32_t depth, TextureFormat requestFormat, bool mipmap, const void* initialData) override;
+    virtual Ref<ITexture> onCreateRenderTarget(uint32_t width, uint32_t height, TextureFormat requestFormat, bool mipmap) override;
+    virtual Ref<IDepthBuffer> onCreateDepthBuffer(uint32_t width, uint32_t height) override;
+    virtual Ref<ISamplerState> onCreateSamplerState(const SamplerStateData& desc) override;
+    virtual Ref<IShaderPass> onCreateShaderPass(const byte_t* vsCode, int vsCodeLen, const byte_t* fsCodeLen, int psCodeLen, ShaderCompilationDiag* diag) override;
+    virtual void onUpdatePipelineState(const BlendStateDesc& blendState, const RasterizerStateDesc& rasterizerState, const DepthStencilStateDesc& depthStencilState) override;
+    virtual void onUpdateFrameBuffers(ITexture** renderTargets, int renderTargetsCount, IDepthBuffer* depthBuffer) override;
+    virtual void onUpdateRegionRects(const RectI& viewportRect, const RectI& scissorRect, const SizeI& targetSize) override;
+    virtual void onUpdatePrimitiveData(IVertexDeclaration* decls, IVertexBuffer** vertexBuufers, int vertexBuffersCount, IIndexBuffer* indexBuffer) override;
+    virtual void onUpdateShaderPass(IShaderPass* newPass) override;
+    virtual void onClearBuffers(ClearFlags flags, const Color& color, float z, uint8_t stencil) override;
+    virtual void onDrawPrimitive(PrimitiveType primitive, int startVertex, int primitiveCount) override;
+    virtual void onDrawPrimitiveIndexed(PrimitiveType primitive, int startIndex, int primitiveCount) override;
+    virtual void onPresent(ISwapChain* swapChain) override;
+};
+
+class VulkanSwapChain
+    : public ISwapChain
+{
+public:
+    VulkanSwapChain();
+    virtual ~VulkanSwapChain();
+    virtual void dispose() override;
+    virtual ITexture* getColorBuffer() const override;
+
+private:
+};
+
+class VulkanVertexDeclaration
+    : public IVertexDeclaration
+{
+public:
+    VulkanVertexDeclaration();
+    virtual ~VulkanVertexDeclaration();
+    void init(const VertexElement* elements, int elementsCount);
+    virtual void dispose() override;
+
+private:
+};
+
+class VulkanVertexBuffer
+    : public IVertexBuffer
+{
+public:
+    VulkanVertexBuffer();
+    virtual ~VulkanVertexBuffer();
+    void init(GraphicsResourceUsage usage, size_t bufferSize, const void* initialData);
+    virtual void dispose() override;
+    virtual size_t getBytesSize() override;
+    virtual GraphicsResourceUsage usage() const override;
+    virtual void setSubData(size_t offset, const void* data, size_t length) override;
+    virtual void* map() override;
+    virtual void unmap() override;
+
+private:
+};
+
+class VulkanIndexBuffer
+    : public IIndexBuffer
+{
+public:
+    VulkanIndexBuffer();
+    virtual ~VulkanIndexBuffer();
+    void init(GraphicsResourceUsage usage, IndexBufferFormat format, int indexCount, const void* initialData);
+    virtual void dispose() override;
+    virtual size_t getBytesSize() override;
+    virtual GraphicsResourceUsage usage() const override;
+    virtual void setSubData(size_t offset, const void* data, size_t length) override;
+    virtual void* map() override;
+    virtual void unmap() override;
+
+private:
+};
+
+class VulkanTextureBase
+    : public ITexture
+{
+public:
+    VulkanTextureBase();
+    virtual ~VulkanTextureBase();
+};
+
+class VulkanTexture2D
+    : public VulkanTextureBase
+{
+public:
+    VulkanTexture2D();
+    virtual ~VulkanTexture2D();
+    void init(uint32_t width, uint32_t height, TextureFormat requestFormat, bool mipmap, const void* initialData);
+    virtual void dispose() override;
+
+    virtual DeviceTextureType type() const override;
+    virtual void readData(void* outData) override;
+    virtual const SizeI& realSize() override;
+    virtual TextureFormat getTextureFormat() const override;
+    virtual void setSubData(int x, int y, int width, int height, const void* data, size_t dataSize) override;
+    virtual void setSubData3D(int x, int y, int z, int width, int height, int depth, const void* data, size_t dataSize) override;
+
+private:
+};
+
+class VulkanTexture3D
+    : public VulkanTextureBase
+{
+public:
+    VulkanTexture3D();
+    virtual ~VulkanTexture3D();
+    void init(uint32_t width, uint32_t height, uint32_t depth, TextureFormat requestFormat, bool mipmap, const void* initialData);
+    virtual void dispose() override;
+
+    virtual DeviceTextureType type() const override;
+    virtual void readData(void* outData) override;
+    virtual const SizeI& realSize() override;
+    virtual TextureFormat getTextureFormat() const override;
+    virtual void setSubData(int x, int y, int width, int height, const void* data, size_t dataSize) override;
+    virtual void setSubData3D(int x, int y, int z, int width, int height, int depth, const void* data, size_t dataSize) override;
+
+private:
+};
+
+class VulkanRenderTargetTexture
+    : public VulkanTextureBase
+{
+public:
+    VulkanRenderTargetTexture();
+    virtual ~VulkanRenderTargetTexture();
+    void init(uint32_t width, uint32_t height, TextureFormat requestFormat, bool mipmap);
+    virtual void dispose() override;
+
+    virtual DeviceTextureType type() const override;
+    virtual void readData(void* outData) override;
+    virtual const SizeI& realSize() override;
+    virtual TextureFormat getTextureFormat() const override;
+    virtual void setSubData(int x, int y, int width, int height, const void* data, size_t dataSize) override;
+    virtual void setSubData3D(int x, int y, int z, int width, int height, int depth, const void* data, size_t dataSize) override;
+
+private:
+};
+
+class VulkanDepthBuffer
+    : public IDepthBuffer
+{
+public:
+    VulkanDepthBuffer();
+    virtual ~VulkanDepthBuffer();
+    void init(uint32_t width, uint32_t height);
+    virtual void dispose() override;
+
+private:
+};
+
+class VulkanSamplerState
+    : public ISamplerState
+{
+public:
+    VulkanSamplerState();
+    virtual ~VulkanSamplerState();
+    void init(const SamplerStateData& desc);
+    virtual void dispose() override;
+
+private:
+};
+
+class VulkanShaderPass
+    : public IShaderPass
+{
+public:
+    VulkanShaderPass();
+    virtual ~VulkanShaderPass();
+    void init(VulkanDeviceContext* context, const byte_t* vsCode, int vsCodeLen, const byte_t* fsCodeLen, int psCodeLen, ShaderCompilationDiag* diag);
+    virtual void dispose() override;
+
+    virtual int getUniformCount() const override;
+    virtual IShaderUniform* getUniform(int index) const override;
+    virtual void setUniformValue(int index, const void* data, size_t size) override;
+    virtual int getUniformBufferCount() const override;
+    virtual IShaderUniformBuffer* getUniformBuffer(int index) const override;
+    virtual IShaderSamplerBuffer* samplerBuffer() const override;
+
+private:
+};
+
+class VulkanShaderUniformBuffer
+    : public IShaderUniformBuffer
+{
+public:
+    VulkanShaderUniformBuffer();
+    virtual ~VulkanShaderUniformBuffer();
+    void init();
+    virtual void dispose() override;
+
+    virtual const std::string& name() const override;
+    virtual int getUniformCount() const override;
+    virtual IShaderUniform* getUniform(int index) const override;
+    virtual size_t bufferSize() const override;
+    virtual void setData(const void* data, size_t size) override;
+
+private:
+    std::string m_name;
+};
+
+class VulkanShaderUniform
+    : public IShaderUniform
+{
+public:
+    VulkanShaderUniform();
+    virtual ~VulkanShaderUniform();
+    void init();
+    virtual void dispose() override;
+    virtual const ShaderUniformTypeDesc& desc() const override { return m_desc; }
+    virtual const std::string& name() const override { return m_name; }
+
+private:
+    ShaderUniformTypeDesc m_desc;
+    std::string m_name;
+};
+
+class VulkanLocalShaderSamplerBuffer
+    : public IShaderSamplerBuffer
+{
+public:
+    VulkanLocalShaderSamplerBuffer();
+    virtual ~VulkanLocalShaderSamplerBuffer();
+    void init();
+    virtual void dispose() override;
+
+    virtual int registerCount() const override;
+    virtual const std::string& getTextureRegisterName(int registerIndex) const override;
+    virtual const std::string& getSamplerRegisterName(int registerIndex) const override;
+    virtual void setTexture(int registerIndex, ITexture* texture) override;
+    virtual void setSamplerState(int registerIndex, ISamplerState* state) override;
+
+private:
+};
+
+} // namespace detail
+} // namespace ln
+
