36a735e23d0966e0b473d3f195b7018951d8e52b
lriki <lriki.net@gmail.com>
2019-04-07 20:20:58 +0900

    vulkan headers

--- external/ImportExternalLibraries.cmake
+++ external/ImportExternalLibraries.cmake
@@ -355,17 +355,23 @@ list(APPEND LN_EXTERNAL_LIBS Box2D)
 #--------------------------------------
 # Vulkan
 
-if (LN_USE_VULKAN)
-    if (WIN32)
-        find_package(Vulkan REQUIRED)
-        #list(APPEND LN_EXTERNAL_LIBS Vulkan::Vulkan)
-        
-        # IMPORTED がないと install で Target not found になる
-        add_library(VulkanImported IMPORTED INTERFACE)
-        #set_target_properties(VulkanImported PROPERTIES IMPORTED_LOCATION_RELEASE ${Vulkan_LIBRARIES})
-        #set_target_properties(VulkanImported PROPERTIES IMPORTED_LOCATION_DEBUG ${Vulkan_LIBRARIES})
-        # dynamic load としたため、include だけでよい
-        set_target_properties(VulkanImported PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${Vulkan_INCLUDE_DIRS})
-        list(APPEND LN_EXTERNAL_LIBS VulkanImported)
-    endif()
-endif()
+#if (LN_USE_VULKAN)
+##    if (WIN32)
+ln_make_external_find_path(VulkanHeaders_ROOT "Vulkan-Headers")
+
+add_library(VulkanHeaders IMPORTED INTERFACE)   # IMPORTED がないと install で Target not found になる
+set_target_properties(VulkanHeaders PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${VulkanHeaders_ROOT}/include)
+list(APPEND LN_EXTERNAL_LIBS VulkanHeaders)
+
+#find_package(Vulkan REQUIRED)
+#list(APPEND LN_EXTERNAL_LIBS Vulkan::Vulkan)
+
+
+#add_library(VulkanImported IMPORTED INTERFACE)
+#set_target_properties(VulkanImported PROPERTIES IMPORTED_LOCATION_RELEASE ${Vulkan_LIBRARIES})
+#set_target_properties(VulkanImported PROPERTIES IMPORTED_LOCATION_DEBUG ${Vulkan_LIBRARIES})
+# dynamic load としたため、include だけでよい
+#set_target_properties(VulkanImported PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${Vulkan_INCLUDE_DIRS})
+#list(APPEND LN_EXTERNAL_LIBS VulkanImported)
+#    endif()
+#endif()
