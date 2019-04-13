05ebb3eeab2ed6d5f0751efb3eee2b9c4edee5e6
lriki <lriki.net@gmail.com>
2019-04-13 15:21:02 +0900

    ui

---
+++ src/LuminoEngine/src/UI/UICard.cpp
@@ -0,0 +1,26 @@
+﻿
+#include "Internal.hpp"
+#include <LuminoEngine/UI/UICard.hpp>
+
+namespace ln {
+
+//==============================================================================
+// UICard
+// https://material-ui.com/demos/cards/
+
+Ref<UICard> UICard::create()
+{
+    return newObject<UICard>();
+}
+
+UICard::UICard()
+{
+}
+
+void UICard::init()
+{
+    UIContainerElement::init();
+}
+
+} // namespace ln
+
