43ca543ca5e5107d2d6b94f4e731348f6d54ef82
lriki <lriki.net@gmail.com>
2019-04-25 18:02:00 +0900

    code format

--- include/LuminoEngine/Graphics/GraphicsResource.hpp
+++ include/LuminoEngine/Graphics/GraphicsResource.hpp
@@ -1,4 +1,5 @@
-﻿#pragma once
+﻿// Copyright (c) 2019+ lriki. Distributed under the MIT license.
+#pragma once
 #include "Common.hpp"
 
 namespace ln {
@@ -8,46 +9,45 @@ class GraphicsResourceInternal;
 
 /** Graphics 機能に関係するリソースのベースクラスです。*/
 class LN_API GraphicsResource
-	: public Object
+    : public Object
 {
 protected:
-	virtual void onDispose(bool explicitDisposing) override;
-	virtual void onChangeDevice(detail::IGraphicsDevice* device) = 0;
+    virtual void onDispose(bool explicitDisposing) override;
+    virtual void onChangeDevice(detail::IGraphicsDevice* device) = 0;
 
 LN_CONSTRUCT_ACCESS:
-	GraphicsResource();
-	virtual ~GraphicsResource();
-	void init();
+    GraphicsResource();
+    virtual ~GraphicsResource();
+    void init();
 
 private:
-	detail::GraphicsManager* m_manager;
+    detail::GraphicsManager* m_manager;
 
-	friend class detail::GraphicsResourceInternal;
+    friend class detail::GraphicsResourceInternal;
 };
-
 
 namespace detail {
 
 class GraphicsResourceInternal
 {
 public:
-	static IndexBufferFormat selectIndexBufferFormat(int vertexCount) { return (vertexCount > 0xFFFF) ? IndexBufferFormat::UInt32 : IndexBufferFormat::UInt16; }
+    static IndexBufferFormat selectIndexBufferFormat(int vertexCount) { return (vertexCount > 0xFFFF) ? IndexBufferFormat::UInt32 : IndexBufferFormat::UInt16; }
 
-	static detail::GraphicsManager* manager(GraphicsResource* obj) { return obj->m_manager; }
+    static detail::GraphicsManager* manager(GraphicsResource* obj) { return obj->m_manager; }
 
-	template<class TReturn, class TObject>
-	static TReturn* resolveRHIObject(const TObject& obj, bool* outModified)
-	{
-		bool modified = false;
-		TReturn* rhi = nullptr;
-		if (obj) {
-			rhi = obj->resolveRHIObject(&modified);
-		}
-		if (outModified) {
-			*outModified = modified;
-		}
-		return rhi;
-	}
+    template<class TReturn, class TObject>
+    static TReturn* resolveRHIObject(const TObject& obj, bool* outModified)
+    {
+        bool modified = false;
+        TReturn* rhi = nullptr;
+        if (obj) {
+            rhi = obj->resolveRHIObject(&modified);
+        }
+        if (outModified) {
+            *outModified = modified;
+        }
+        return rhi;
+    }
 };
 
 } // namespace detail
