05f9ac658f1bad0fd59e41eb89cb74f24e260c20
lriki <lriki.net@gmail.com>
2019-04-30 14:24:36 +0900

    binding

--- tools/Editor/src/main.cpp
+++ tools/Editor/src/main.cpp
@@ -1,5 +1,20 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+class ViewModel : public QObject
+{
+    Q_OBJECT
+public:
+    //Q_INVOKABLE QDateTime getCurrentDateTime() const {
+    //    return QDateTime::currentDateTime();
+    //}
+
+    Q_INVOKABLE void clicked()
+    {
+        printf("\n");
+    }
+};
 
 int main(int argc, char *argv[])
 {
@@ -9,7 +24,16 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    QObject* obj = engine.rootObjects().front();
+
+    ViewModel vm;
+    engine.rootContext()->setContextProperty("vm", &vm);
+
+
 	    
     return app.exec();
 }
+
+#include "main.moc"
 
--- tools/Editor/src/main.qml
+++ tools/Editor/src/main.qml
@@ -1,9 +1,28 @@
 import QtQuick 2.3
-import QtQuick.Window 2.2
+import QtQuick.Controls 1.4
 
 // main window
-Window {	
+ApplicationWindow {
     visible: true
+
+    menuBar: MenuBar {
+        Menu {
+            title: "File"
+            MenuItem { text: "Open..." }
+            MenuItem {
+                text: "Close"
+                onTriggered: { vm.clicked(); }
+            }
+        }
+
+        Menu {
+            title: "Edit"
+            MenuItem { text: "Cut" }
+            MenuItem { text: "Copy" }
+            MenuItem { text: "Paste" }
+        }
+    }
+
     MouseArea {
         anchors.fill: parent
         onClicked: {
