8ffc912853f9ac8798a89d1188213c48e85ce3bc
lriki <lriki.net@gmail.com>
2019-05-26 21:56:12 +0900

    editor

---
+++ src/LuminoEditor/Studio/SceneContentsView.cpp
@@ -0,0 +1,18 @@
+﻿
+#include "MainWindow.h"
+#include "SceneContentsView.h"
+
+//==============================================================================
+// SceneContentsViewProvider
+
+SceneContentsViewProvider::SceneContentsViewProvider()
+    : m_treeView(nullptr)
+{
+
+}
+
+QWidget* SceneContentsViewProvider::createView()
+{
+    LN_NOTIMPLEMENTED();
+    return nullptr;
+}
---
+++ src/LuminoEditor/Studio/SceneContentsView.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "ContentsViewManager.h"
+
+class SceneContentsViewProvider : public ContentsViewProvider
+{
+public:
+    virtual ln::String icon() const override { return u"music"; }
+    virtual QWidget* createView() override;
+
+LN_CONSTRUCT_ACCESS:
+    SceneContentsViewProvider();
+
+private:
+    SceneContentsViewProvider* m_treeView;
+};
+
