9905b1a448648ddabdef4612e111697a41b30949
lriki <lriki.net@gmail.com>
2019-07-26 12:35:36 +0900

    add files

---
+++ include/LuminoEngine/Graphics/CommandBuffer.hpp
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2019+ lriki. Distributed under the MIT license.
+#pragma once
+#include "GraphicsResource.hpp"
+
+namespace ln {
+namespace detail {
+class ICommandList;
+}
+
+/**  */
+class CommandBuffer
+    : public GraphicsResource
+{
+public:
+
+protected:
+    virtual void onDispose(bool explicitDisposing) override;
+    virtual void onChangeDevice(detail::IGraphicsDevice* device) override;
+
+LN_CONSTRUCT_ACCESS:
+	CommandBuffer();
+    virtual ~CommandBuffer();
+
+    /** @copydoc create(int, int) */
+    void init();
+
+private:
+    detail::ICommandList* resolveRHIObject(GraphicsContext* context, bool* outModified);
+
+    Ref<detail::ICommandList> m_rhiObject;
+
+    friend class detail::GraphicsResourceInternal;
+};
+
+} // namespace ln
---
+++ src/LuminoEngine/src/Graphics/RenderPass.cpp
@@ -0,0 +1,41 @@
+﻿
+#include "Internal.hpp"
+#include "GraphicsManager.hpp"
+#include "GraphicsDeviceContext.hpp"
+#include <LuminoEngine/Graphics/RenderPass.hpp>
+
+namespace ln {
+
+//==============================================================================
+// RenderPass
+
+RenderPass::RenderPass()
+    : m_rhiObject()
+{
+}
+
+RenderPass::~RenderPass()
+{
+}
+
+void RenderPass::init()
+{
+    GraphicsResource::init();
+}
+
+void RenderPass::onDispose(bool explicitDisposing)
+{
+    m_rhiObject = nullptr;
+    GraphicsResource::onDispose(explicitDisposing);
+}
+
+void RenderPass::onChangeDevice(detail::IGraphicsDevice* device)
+{
+}
+
+detail::IRenderPass* RenderPass::resolveRHIObject(GraphicsContext* context, bool* outModified)
+{
+	return nullptr;
+}
+
+} // namespace ln
