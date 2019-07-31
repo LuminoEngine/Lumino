56f6afc97bd55c531476164b39dc2a64eb159f23
lriki <lriki.net@gmail.com>
2019-07-31 17:51:18 +0900

    doc

--- src/LuminoEngine/src/Graphics/RenderPass.cpp
+++ src/LuminoEngine/src/Graphics/RenderPass.cpp
@@ -1,4 +1,10 @@
-﻿
+﻿/*
+	移行 Note:
+		map を各 GraphicsResource から CommandBuffer へ移動する。
+		※ ネイティブの map (transfar) は RenderPass の外側でなければ使えないので、順序制御するため CommandBuffer に統合したい
+
+		CommandBuffer と RenderPass は全て pool からインスタンスを得る。インスタンスを外側で保持し続けてはならない。
+*/
 #include "Internal.hpp"
 #include "GraphicsManager.hpp"
 #include "GraphicsDeviceContext.hpp"
