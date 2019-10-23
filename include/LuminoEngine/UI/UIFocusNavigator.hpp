e809b895736b6e2dc34dbbda7ecf8d009fb2c415
lriki <lriki.net@gmail.com>
2019-10-23 12:56:54 +0900

    UIFocusNavigator

---
+++ include/LuminoEngine/UI/UIFocusNavigator.hpp
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include "UIContainerElement.hpp"
+
+namespace ln {
+class Material;
+
+/**
+ * 要素のフォーカス遷移を管理すためのコンテナ要素。
+ *
+ * Dialog のようにひとつの Active な要素にのみ、UI イベントを通知するようにしたいときに使う。
+ * Dialog と異なるのは、関係する要素を常に表示したままにすることができること。
+ *
+ * 基本動作
+ * ----------
+ * 例えば RPG の装備画面を考えてみる。
+ * - 左側にはキャラクターの装備しているウィンドウ
+ * - 右側には持ち物一覧ウィンドウ
+ * この2つは最初から表示されているので、Navigator の子要素として最初から追加しておく。
+ *
+ * 最初は gotoFocus() で装備ウィンドウにフォーカスを当てておく。
+ * 装備ウィンドウはキーやマウスで操作できるが、持ち物ウィンドウにはイベントが回らなくなるため一切操作はできなくなる。
+ *
+ * 装備ウィンドウで決定ボタンなどを押したら、pushFocus() で持ち物ウィンドウへのフォーカスをスタックに積む。
+ * これで持ち物ウィンドウが操作可能になり、装備ウィンドウは操作できなくなる。
+ *
+ * 持ち物ウィンドウで決定やキャンセルを押したら、popFocus() で装備ウィンドウにフォーカスを戻すことができる。
+ *
+ * その他の動作
+ * ----------
+ * 初期子要素以外の UIElement を goto, push に指定することもできる。
+ * その場合その時点で子要素として追加され、フォーカスと操作は管理下に置かれる。
+ *
+ *
+ */
+class UIFocusNavigator
+    : public UIContainerElement
+{
+public:
+	void gotoFocus(UIElement* element);
+	void pushFocus(UIElement* element);
+	void popFocus();
+	void clearFocusHistory();
+
+LN_CONSTRUCT_ACCESS:
+	UIFocusNavigator();
+	void init();
+
+protected:
+
+private:
+};
+
+} // namespace ln
+
---
+++ src/LuminoEngine/src/UI/UIFocusNavigator.cpp
@@ -0,0 +1,36 @@
+﻿
+#include "Internal.hpp"
+#include <LuminoEngine/UI/UIFocusNavigator.hpp>
+
+namespace ln {
+
+//==============================================================================
+// UIFocusNavigator
+
+UIFocusNavigator::UIFocusNavigator()
+{
+}
+
+void UIFocusNavigator::init()
+{
+    UIContainerElement::init();
+}
+
+void UIFocusNavigator::gotoFocus(UIElement* element)
+{
+}
+
+void UIFocusNavigator::pushFocus(UIElement* element)
+{
+}
+
+void UIFocusNavigator::popFocus()
+{
+}
+
+void UIFocusNavigator::clearFocusHistory()
+{
+}
+
+} // namespace ln
+
