be46c33228e9b3591a796b74b9c834d7ad240562
lriki <lriki.net@gmail.com>
2019-12-06 12:53:05 +0900

    test

--- docs/WebDocument/template/styles/main.js
+++ docs/WebDocument/template/styles/main.js
@@ -1,11 +1,5 @@
 
-console.log("my main.");
 var element = document.getElementById("navbar-revision-box");
-console.log(element);
-
-console.log(location.href);
-console.log(location.href.includes("localhost"));
-console.log(location.href.includes("latest-lumino-webdocs"));
 
 if (location.href.includes("latest-lumino-webdocs")) {
     element.style.display = "block";
