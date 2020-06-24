97efd09dd2a185fe29e19debd14cfca6775b0c7e
lriki <lriki.net@gmail.com>
2020-06-24 12:26:08 +0900

    Added AssetObject

---
+++ include/LuminoEngine/Asset/AssetObject.hpp
@@ -0,0 +1,27 @@
+﻿
+#pragma once
+#include "Common.hpp"
+#include "../Engine/Object.hpp"
+
+namespace ln {
+
+/**
+ * アセットファイルやその他の外部ファイルをインポートして構築されるオブジェクトのベースクラスです。
+ */
+LN_CLASS()
+class AssetObject
+    : public Object
+{
+	LN_OBJECT;
+public:
+
+protected:
+
+LN_CONSTRUCT_ACCESS:
+    AssetObject();
+    bool init();
+
+private:
+};
+
+} // namespace ln
---
+++ src/LuminoEngine/src/Asset/AssetObject.cpp
@@ -0,0 +1,24 @@
+﻿
+#include "Internal.hpp"
+#include "AssetManager.hpp"
+#include <LuminoEngine/Asset/AssetObject.hpp>
+
+namespace ln {
+
+//=============================================================================
+// AssetObject
+
+LN_OBJECT_IMPLEMENT(AssetObject, Object) {}
+
+AssetObject::AssetObject()
+{
+}
+
+bool AssetObject::init()
+{
+	return Object::init();
+}
+
+
+} // namespace ln
+
