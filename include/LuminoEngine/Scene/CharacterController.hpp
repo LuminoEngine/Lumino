2488dcd4eeb6b1ac618a2fa432cceb9f99eee37c
lriki <lriki.net@gmail.com>
2020-07-21 12:58:43 +0900

    Added CharacterController

---
+++ include/LuminoEngine/Scene/CharacterController.hpp
@@ -0,0 +1,42 @@
+﻿
+#pragma once
+#include "Component.hpp"
+
+namespace ln {
+class InputController;
+
+class CharacterController
+	: public Component
+{
+public:
+
+protected:
+	void onUpdate(float elapsedSeconds) override;
+
+LN_CONSTRUCT_ACCESS:
+	CharacterController();
+	bool init();
+
+private:
+	struct InputState
+	{
+		bool moveForward;
+		bool moveBack;
+		bool turnRight;
+		bool turnLeft;
+		bool sneak;
+
+		InputState();
+		void reset();
+	};
+
+	// 複数プレイヤーをマルチビューで操作するときなどを考慮すると、
+	// UIViewport の onKeyDown とかにアタッチするよりは
+	// VirtualController に直接アタッチしてしまった方がシンプルに実装できる。
+	// キーコンフィグも有効になるし。
+	InputController* m_inputController;
+
+	InputState m_inputState;
+};
+
+} // namespace ln
---
+++ src/LuminoEngine/src/Scene/CharacterController.cpp
@@ -0,0 +1,45 @@
+﻿
+#include "Internal.hpp"
+#include <LuminoEngine/Scene/CharacterController.hpp>
+#include "../Input/InputManager.hpp"
+
+namespace ln {
+    
+//==============================================================================
+// CharacterController
+
+CharacterController::CharacterController()
+    : m_inputController(nullptr)
+{
+}
+
+bool CharacterController::init()
+{
+    if (!Component::init()) return false;
+
+    m_inputController = detail::EngineDomain::inputManager()->getVirtualPad(0);
+
+
+    return true;
+}
+
+void CharacterController::onUpdate(float elapsedSeconds)
+{
+
+}
+
+CharacterController::InputState::InputState()
+{
+	reset();
+}
+
+void CharacterController::InputState::reset()
+{
+	moveForward = false;
+	moveBack = false;
+	turnRight = false;
+	turnLeft = false;
+	sneak = false;
+}
+} // namespace ln
+
