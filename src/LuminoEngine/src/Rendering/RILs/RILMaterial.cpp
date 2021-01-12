e2c64718befc9087c5293b87ccc97979a6ce0aa8
lriki <lriki.net@gmail.com>
2021-01-12 12:52:11 +0900

    material

---
+++ src/LuminoEngine/src/Rendering/RILs/RILMaterial.cpp
@@ -0,0 +1,38 @@
+﻿
+#include "Internal.hpp"
+#include "RILMaterial.hpp"
+
+namespace ln {
+namespace detail {
+
+RILMaterial::RILMaterial()
+{
+	reset();
+}
+
+void RILMaterial::reset()
+{
+	material = nullptr;
+	blendMode = BlendMode::Normal;
+	cullingMode = CullMode::Front;
+	depthTestEnabled = true;
+	depthWriteEnabled = true;
+	shadingModel = ShadingModel::Default;
+	primitiveTopology = PrimitiveTopology::TriangleList;
+}
+
+bool RILMaterial::equals(const RILMaterial* other) const
+{
+	return
+		material == other->material &&  // TODO: Material が一致することはまずない。ちゃんと中身かhashを見ること。
+		blendMode == other->blendMode &&
+		cullingMode == other->cullingMode &&
+		depthTestEnabled == other->depthTestEnabled &&
+		depthWriteEnabled == other->depthWriteEnabled &&
+		shadingModel == other->shadingModel &&
+		primitiveTopology == other->primitiveTopology;
+}
+
+} // namespace detail
+} // namespace ln
+
---
+++ src/LuminoEngine/src/Rendering/RILs/RILMaterial.hpp
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "../RenderStage.hpp"
+
+namespace ln {
+namespace detail {
+
+// Material, RenderingContext(VisualNode), RenderFeature, SceneRenderPass のマージ結果。
+//
+// 優先度は SceneRenderPass > RenderFeature > RenderingContext (> VisualNode) > Material
+// DrawElementList の構築時点では RenderingContext と Material のマージ結果を保持し、
+// その後 SceneRenderer の中で、Batch リスト構築時に RenderFeature、Pass 描画時に SceneRenderPass の分をマージして、最終的なマテリアルとする。
+//
+// このような実装に至った背景
+// ----------
+// 以前は RenderState は DrawElementList の構築時点で確定すると思っていたが、
+// RenderFeature の都合 (Instancing 使いたい、シャドウマップ作るときは両面カリングにしたい、等) で、
+// より低いレイヤーでも変更する必要が出てきた。
+// (特にシャドウマップは R32 フォーマットにしているが、適切に RenderState を指定しないと Vulkan の検証レイヤーに指摘される)
+struct RILMaterial	// 旧: GeometryStageParameters
+{
+public:
+	Material* material;		// ソースマテリアル。Mesh を描画するときは、Mesh にアタッチされた Material.
+	BlendMode blendMode;
+	CullMode cullingMode;
+	bool depthTestEnabled;
+	bool depthWriteEnabled;
+	ShadingModel shadingModel;
+	PrimitiveTopology primitiveTopology;
+
+	RILMaterial();
+	void reset();
+	bool equals(const RILMaterial* other) const;
+};
+
+} // namespace detail
+} // namespace ln
+
