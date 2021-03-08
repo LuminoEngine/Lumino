87d5a640d01cd845c13f643981d0c1977227ffad
lriki <lriki.net@gmail.com>
2021-03-08 21:51:40 +0900

    RLIBatchMaterial

---
+++ src/LuminoEngine/src/Rendering/RLIs/RLIBatchMaterial.cpp
@@ -0,0 +1,40 @@
+﻿
+#include "Internal.hpp"
+#include <LuminoEngine/Graphics/GraphicsContext.hpp>
+#include <LuminoEngine/Rendering/Material.hpp>
+#include "RLIBatchMaterial.hpp"
+
+namespace ln {
+namespace detail {
+
+RLIBatchMaterial::RLIBatchMaterial()
+{
+	reset();
+}
+
+void RLIBatchMaterial::reset()
+{
+	material = nullptr;
+	blendMode = BlendMode::Normal;
+	cullingMode = CullMode::Front;
+	depthTestEnabled = true;
+	depthWriteEnabled = true;
+	shadingModel = ShadingModel::Default;
+	primitiveTopology = PrimitiveTopology::TriangleList;
+}
+
+bool RLIBatchMaterial::equals(const RLIBatchMaterial* other) const
+{
+	return
+		material == other->material &&  // TODO: Material が一致することはまずない。ちゃんと中身かhashを見ること。
+		blendMode == other->blendMode &&
+		cullingMode == other->cullingMode &&
+		depthTestEnabled == other->depthTestEnabled &&
+		depthWriteEnabled == other->depthWriteEnabled &&
+		shadingModel == other->shadingModel &&
+		primitiveTopology == other->primitiveTopology;
+}
+
+} // namespace detail
+} // namespace ln
+
---
+++ src/LuminoEngine/src/Rendering/RLIs/RLIBatchMaterial.hpp
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "../RenderStage.hpp"
+
+namespace ln {
+namespace detail {
+
+// RLIMaterial は本当に最終的に使われるマテリアル情報だが、こちらは Batch に付く情報。
+// RLIBatchMaterial は上位レイヤーから要求された情報を保持し、複数の renderPass 間で参照される const 情報。
+// RLIBatchMaterial は最終的に RenderPass の都合で設定がオーバーライドされることがあり、その結果が RLIMaterial となる。
+struct RLIBatchMaterial
+{
+public:
+	Material* material;		// ソースマテリアル。Mesh を描画するときは、Mesh にアタッチされた Material.
+	BlendMode blendMode;
+	CullMode cullingMode;
+	bool depthTestEnabled;
+	bool depthWriteEnabled;
+	ShadingModel shadingModel;
+	PrimitiveTopology primitiveTopology;
+
+	RLIBatchMaterial();
+	void reset();
+	bool equals(const RLIBatchMaterial* other) const;
+};
+
+} // namespace detail
+} // namespace ln
+
