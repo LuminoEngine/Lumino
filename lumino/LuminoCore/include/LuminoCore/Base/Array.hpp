850ea2c5b0cbb9110453664c80f1de126d824274
lriki <lriki.net@gmail.com>
2021-09-13 19:08:08 +0900

    array

---
+++ lumino/LuminoCore/include/LuminoCore/Base/Array.hpp
@@ -0,0 +1,180 @@
+﻿// Copyright (c) 2018+ lriki. Distributed under the MIT license.
+#pragma once
+
+#include <vector>
+#include <algorithm>
+#include "Assertion.hpp"
+#include "StlHelper.hpp"
+#include "Optional.hpp"
+
+namespace ln {
+
+/**
+ * 動的配列
+ * 
+ * std::vector と似ていますが、いくつかの便利なメンバ関数やエラーを記録する機能が追加されています。
+ * また std::vector と同様、メモリの連続性が保証されています。
+ */
+template<typename T>
+class Array
+{
+public:
+    using container_type = std::vector<T>;
+    using value_type = typename container_type::value_type;
+    using pointer = typename container_type::pointer;
+    using const_pointer = typename container_type::const_pointer;
+    using reference = typename container_type::reference;
+    using const_reference = typename container_type::const_reference;
+    using iterator = typename container_type::iterator;
+    using const_iterator = typename container_type::const_iterator;
+    using reverse_iterator = typename container_type::reverse_iterator;
+    using const_reverse_iterator = typename container_type::const_reverse_iterator;
+    using size_type = int;// typename container_type::size_type;
+    using difference_type = typename container_type::difference_type;
+    using allocator_type = typename container_type::allocator_type;
+
+
+    /** Construct empty array. */
+    Array() = default;
+
+    /** Copy the array and Constructs a new array. */
+    Array(const Array& ary) = default;
+
+    /** Copy the array and Constructs a new array. */
+    Array(const container_type& ary);
+
+    /** Constructor with move. */
+    Array(Array&& ary) = default;
+
+    /** Constructor with move. */
+    Array(container_type&& ary);
+
+    /** Constructor with initializer_list. */
+    Array(std::initializer_list<T> list);
+
+    /** Range constructor. */
+    template<class TIter>
+    Array(TIter begin, TIter end);
+
+    /** Destructor.  */
+    ~Array() = default;
+
+    /** Assign content. */
+    Array& operator=(const Array&) = default;
+
+    /** Assign content. */
+    Array& operator=(Array&&) = default;
+
+    /** Assign content. */
+    Array& operator=(const container_type& other);
+
+    /** Assign content. */
+    Array& operator=(container_type&& other);
+
+    /** Assign content. */
+    Array& operator=(std::initializer_list<value_type> list);
+
+    /** @defgroup Iterators: */
+    /** @{ */
+
+    [[nodiscard]]
+    iterator begin() noexcept;
+
+    [[nodiscard]]
+    iterator end() noexcept;
+
+    [[nodiscard]]
+    const_iterator begin() const noexcept;
+
+    [[nodiscard]]
+    const_iterator end() const noexcept;
+
+    [[nodiscard]]
+    const_iterator cbegin() const noexcept;
+
+    [[nodiscard]]
+    const_iterator cend() const noexcept;
+
+    /** @} */
+    /** @defgroup Capacity */
+    /** @{ */
+
+    /** Return size. */
+    [[nodiscard]]
+    int size() const noexcept;
+
+    /** Return maximum size. */
+    [[nodiscard]]
+    size_t max_size() const noexcept;
+
+    /** Return size of allocated storage capacity. */
+    [[nodiscard]]
+    size_t capacity() const noexcept;
+
+    /** Change size. */
+    void resize(size_type count);
+
+    /** Change size. */
+    void resize(size_type count, const value_type& value);
+
+    /** Request a change in capacity. */
+    void reserve(size_type new_capacity);
+
+    /** Truncate the area to the size of the container. */
+    void shrink_to_fit();
+
+    /** Test whether vector is empty. */
+    [[nodiscard]]
+    bool isEmpty() const noexcept;
+
+    /** Test whether vector is empty. */
+    [[nodiscard]]
+    explicit operator bool() const noexcept;
+
+    /** @} */
+    /** @defgroup  Element acces: */
+    /** @{ */
+
+    /** Access the element at the specified index. */
+    [[nodiscard]]
+    T& at(int index);
+
+    /** Access the element at the specified index. */
+    [[nodiscard]]
+    const T& at(int index) const;
+
+    /** Access the element at the specified index. */
+    [[nodiscard]]
+    T& operator[](int index);
+
+    /** Access the element at the specified index. */
+    [[nodiscard]]
+    const T& operator[](int index) const;
+
+    [[nodiscard]]
+    reference front();
+
+    [[nodiscard]]
+    const_reference front() const;
+
+    [[nodiscard]]
+    reference back();
+
+    [[nodiscard]]
+    const_reference back() const;
+
+    [[nodiscard]]
+    T* data() noexcept;
+
+    [[nodiscard]]
+    const T* data() const noexcept;
+
+    /** @} */
+
+private:
+    container_type m_data;
+};
+
+} // namespace ln
+
+#include "Array.ipp"
---
+++ lumino/LuminoCore/include/LuminoCore/Base/Array.ipp
@@ -0,0 +1,208 @@
+﻿// Copyright (c) 2018+ lriki. Distributed under the MIT license.
+#pragma once
+
+namespace ln {
+
+template<class T>
+inline Array<T>::Array(const container_type& other)
+    : m_data(other)
+{}
+
+template<class T>
+inline Array<T>::Array(container_type&& other)
+    : m_data(std::move(other))
+{}
+
+template<class T>
+inline Array<T>::Array(std::initializer_list<T> list)
+    : m_data(list)
+{
+}
+
+template<class T>
+template<class TIter>
+Array<T>::Array(TIter begin, TIter end)
+    : m_data(begin, end)
+{}
+
+template<class T>
+Array<T>& Array<T>::operator=(const container_type& other)
+{
+    m_data = other;
+    return *this;
+}
+
+template<class T>
+Array<T>& Array<T>::operator=(container_type&& other)
+{
+    m_data = std::move(other);
+    return *this;
+}
+
+template<class T>
+Array<T>& Array<T>::operator=(std::initializer_list<value_type> list)
+{
+    m_data = list;
+    return *this;
+}
+
+
+
+template<class T>
+inline typename Array<T>::iterator Array<T>::begin() noexcept
+{
+    return m_data.begin();
+}
+
+template<class T>
+inline typename Array<T>::iterator Array<T>::end() noexcept
+{
+    return m_data.end();
+}
+
+template<class T>
+inline typename Array<T>::const_iterator Array<T>::begin() const noexcept
+{
+    return m_data.begin();
+}
+
+template<class T>
+inline typename Array<T>::const_iterator Array<T>::end() const noexcept
+{
+    return m_data.end();
+}
+
+template<class T>
+inline typename Array<T>::const_iterator Array<T>::cbegin() const noexcept
+{
+    return m_data.cbegin();
+}
+
+template<class T>
+inline typename Array<T>::const_iterator Array<T>::cend() const noexcept
+{
+    return m_data.cend();
+}
+
+
+
+template<class T>
+inline int Array<T>::size() const noexcept
+{
+    return static_cast<int>(m_data.size());
+}
+
+template<class T>
+inline size_t Array<T>::max_size() const noexcept
+{
+    return std::numeric_limits<int>::max();
+}
+
+template<class T>
+inline size_t Array<T>::capacity() const noexcept
+{
+    return m_data.capacity();
+}
+
+template<class T>
+inline void Array<T>::resize(size_type count)
+{
+    m_data.resize(count);
+}
+
+template<class T>
+inline void Array<T>::resize(size_type count, const value_type& value)
+{
+    m_data.resize(count, value);
+}
+
+template<class T>
+inline void Array<T>::reserve(size_type new_capacity)
+{
+    m_data.reserve(new_capacity);
+}
+
+template<class T>
+inline void Array<T>::shrink_to_fit()
+{
+    m_data.shrink_to_fit();
+}
+
+template<class T>
+inline bool Array<T>::isEmpty() const noexcept
+{
+    return m_data.empty();
+}
+
+template<class T>
+inline Array<T>::operator bool() const noexcept
+{
+    return !m_data.empty();
+}
+
+
+
+
+template<class T>
+inline typename T& Array<T>::at(int index)
+{
+    return m_data.at(index);
+}
+
+template<class T>
+inline const T& Array<T>::at(int index) const
+{
+    return m_data.at(index);
+}
+
+template<class T>
+inline T& Array<T>::operator[](int index)
+{
+    return m_data[index];
+}
+
+template<class T>
+inline const T& Array<T>::operator[](int index) const
+{
+    return m_data[index];
+}
+
+template<class T>
+inline typename Array<T>::reference Array<T>::front()
+{
+    return m_data.front();
+}
+
+template<class T>
+inline typename Array<T>::const_reference Array<T>::front() const
+{
+    return m_data.front();
+}
+
+template<class T>
+inline typename Array<T>::reference Array<T>::back()
+{
+    return m_data.back();
+}
+
+template<class T>
+inline typename Array<T>::const_reference Array<T>::back() const
+{
+    return m_data.back();
+}
+
+template<class T>
+inline T* Array<T>::data() noexcept
+{
+    return m_data.data();
+}
+
+template<class T>
+inline const T* Array<T>::data() const noexcept
+{
+    return m_data.data();
+}
+
+} // namespace ln
+
+#include "Array.ipp"
