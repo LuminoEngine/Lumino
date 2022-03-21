bd05abac580f291f124bc5f63063efa21ac74c4c
lriki <lriki.net@gmail.com>
2022-03-21 14:41:35 +0900

    Improved cmake linkage

---
+++ lumino/Bitmap/cmake/LuminoConfig-LuminoBitmap.cmake
@@ -0,0 +1,3 @@
+find_package(ZLIB REQUIRED)
+find_package(libpng CONFIG REQUIRED)
+find_package(nanovg CONFIG REQUIRED)
---
+++ lumino/Platform/cmake/LuminoConfig-LuminoPlatform.cmake
@@ -0,0 +1,2 @@
+find_package(glfw3 CONFIG REQUIRED)
+find_package(glad CONFIG REQUIRED)
