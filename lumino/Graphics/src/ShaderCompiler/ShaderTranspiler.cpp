76942b66d01adf54fff981d520b30abfe735572b
lriki <lriki.net@gmail.com>
2023-02-14 12:25:40 +0900

    Update vcpkg

--- lumino/Graphics/src/ShaderCompiler/ShaderTranspiler.cpp
+++ lumino/Graphics/src/ShaderCompiler/ShaderTranspiler.cpp
@@ -5,11 +5,10 @@
 #include <sstream>
 #include <glad/glad.h>
 #include <glslang/Public/ShaderLang.h>
+#include <glslang/Public/ResourceLimits.h>
 #include <glslang/Include/Types.h>
-#include <glslang/Include/BaseTypes.h>
 #include <glslang/Include/intermediate.h>
 #include <glslang/MachineIndependent/localintermediate.h>
-#include <glslang/Include/ResourceLimits.h>
 #include <glslang/SPIRV/GlslangToSpv.h>
 #include <spirv_cross/spirv_glsl.hpp>
 #include "Lexer/CppLexer.hpp"
@@ -24,115 +23,6 @@
 namespace ln {
 namespace kokage {
 
-// from glslang: StanAalone/ResourceLimits.cpp
-
-const TBuiltInResource DefaultTBuiltInResource = {
-    /* .MaxLights = */ 32,
-    /* .MaxClipPlanes = */ 6,
-    /* .MaxTextureUnits = */ 32,
-    /* .MaxTextureCoords = */ 32,
-    /* .MaxVertexAttribs = */ 64,
-    /* .MaxVertexUniformComponents = */ 4096,
-    /* .MaxVaryingFloats = */ 64,
-    /* .MaxVertexTextureImageUnits = */ 32,
-    /* .MaxCombinedTextureImageUnits = */ 80,
-    /* .MaxTextureImageUnits = */ 32,
-    /* .MaxFragmentUniformComponents = */ 4096,
-    /* .MaxDrawBuffers = */ 32,
-    /* .MaxVertexUniformVectors = */ 128,
-    /* .MaxVaryingVectors = */ 8,
-    /* .MaxFragmentUniformVectors = */ 16,
-    /* .MaxVertexOutputVectors = */ 16,
-    /* .MaxFragmentInputVectors = */ 15,
-    /* .MinProgramTexelOffset = */ -8,
-    /* .MaxProgramTexelOffset = */ 7,
-    /* .MaxClipDistances = */ 8,
-    /* .MaxComputeWorkGroupCountX = */ 65535,
-    /* .MaxComputeWorkGroupCountY = */ 65535,
-    /* .MaxComputeWorkGroupCountZ = */ 65535,
-    /* .MaxComputeWorkGroupSizeX = */ 1024,
-    /* .MaxComputeWorkGroupSizeY = */ 1024,
-    /* .MaxComputeWorkGroupSizeZ = */ 64,
-    /* .MaxComputeUniformComponents = */ 1024,
-    /* .MaxComputeTextureImageUnits = */ 16,
-    /* .MaxComputeImageUniforms = */ 8,
-    /* .MaxComputeAtomicCounters = */ 8,
-    /* .MaxComputeAtomicCounterBuffers = */ 1,
-    /* .MaxVaryingComponents = */ 60,
-    /* .MaxVertexOutputComponents = */ 64,
-    /* .MaxGeometryInputComponents = */ 64,
-    /* .MaxGeometryOutputComponents = */ 128,
-    /* .MaxFragmentInputComponents = */ 128,
-    /* .MaxImageUnits = */ 8,
-    /* .MaxCombinedImageUnitsAndFragmentOutputs = */ 8,
-    /* .MaxCombinedShaderOutputResources = */ 8,
-    /* .MaxImageSamples = */ 0,
-    /* .MaxVertexImageUniforms = */ 0,
-    /* .MaxTessControlImageUniforms = */ 0,
-    /* .MaxTessEvaluationImageUniforms = */ 0,
-    /* .MaxGeometryImageUniforms = */ 0,
-    /* .MaxFragmentImageUniforms = */ 8,
-    /* .MaxCombinedImageUniforms = */ 8,
-    /* .MaxGeometryTextureImageUnits = */ 16,
-    /* .MaxGeometryOutputVertices = */ 256,
-    /* .MaxGeometryTotalOutputComponents = */ 1024,
-    /* .MaxGeometryUniformComponents = */ 1024,
-    /* .MaxGeometryVaryingComponents = */ 64,
-    /* .MaxTessControlInputComponents = */ 128,
-    /* .MaxTessControlOutputComponents = */ 128,
-    /* .MaxTessControlTextureImageUnits = */ 16,
-    /* .MaxTessControlUniformComponents = */ 1024,
-    /* .MaxTessControlTotalOutputComponents = */ 4096,
-    /* .MaxTessEvaluationInputComponents = */ 128,
-    /* .MaxTessEvaluationOutputComponents = */ 128,
-    /* .MaxTessEvaluationTextureImageUnits = */ 16,
-    /* .MaxTessEvaluationUniformComponents = */ 1024,
-    /* .MaxTessPatchComponents = */ 120,
-    /* .MaxPatchVertices = */ 32,
-    /* .MaxTessGenLevel = */ 64,
-    /* .MaxViewports = */ 16,
-    /* .MaxVertexAtomicCounters = */ 0,
-    /* .MaxTessControlAtomicCounters = */ 0,
-    /* .MaxTessEvaluationAtomicCounters = */ 0,
-    /* .MaxGeometryAtomicCounters = */ 0,
-    /* .MaxFragmentAtomicCounters = */ 8,
-    /* .MaxCombinedAtomicCounters = */ 8,
-    /* .MaxAtomicCounterBindings = */ 1,
-    /* .MaxVertexAtomicCounterBuffers = */ 0,
-    /* .MaxTessControlAtomicCounterBuffers = */ 0,
-    /* .MaxTessEvaluationAtomicCounterBuffers = */ 0,
-    /* .MaxGeometryAtomicCounterBuffers = */ 0,
-    /* .MaxFragmentAtomicCounterBuffers = */ 1,
-    /* .MaxCombinedAtomicCounterBuffers = */ 1,
-    /* .MaxAtomicCounterBufferSize = */ 16384,
-    /* .MaxTransformFeedbackBuffers = */ 4,
-    /* .MaxTransformFeedbackInterleavedComponents = */ 64,
-    /* .MaxCullDistances = */ 8,
-    /* .MaxCombinedClipAndCullDistances = */ 8,
-    /* .MaxSamples = */ 4,
-    /* .maxMeshOutputVerticesNV = */ 256,
-    /* .maxMeshOutputPrimitivesNV = */ 512,
-    /* .maxMeshWorkGroupSizeX_NV = */ 32,
-    /* .maxMeshWorkGroupSizeY_NV = */ 1,
-    /* .maxMeshWorkGroupSizeZ_NV = */ 1,
-    /* .maxTaskWorkGroupSizeX_NV = */ 32,
-    /* .maxTaskWorkGroupSizeY_NV = */ 1,
-    /* .maxTaskWorkGroupSizeZ_NV = */ 1,
-    /* .maxMeshViewCountNV = */ 4,
-    /* .maxDualSourceDrawBuffersEXT = */ 1,
-
-    /* .limits = */ {
-        /* .nonInductiveForLoops = */ 1,
-        /* .whileLoops = */ 1,
-        /* .doWhileLoops = */ 1,
-        /* .generalUniformIndexing = */ 1,
-        /* .generalAttributeMatrixVectorIndexing = */ 1,
-        /* .generalVaryingIndexing = */ 1,
-        /* .generalSamplerIndexing = */ 1,
-        /* .generalVariableIndexing = */ 1,
-        /* .generalConstantMatrixVectorIndexing = */ 1,
-    }
-};
 // from glslang/StandAlone/StandAlone.cpp
 // Add things like "#define ..." to a preamble to use in the beginning of the shader.
 class TPreamble {
@@ -369,6 +259,8 @@ bool ShaderCodeTranspiler::compileAndLinkFromHlsl(
     }
     m_shader->addProcesses(preamble.prepro());
 
+     const TBuiltInResource* resource = GetDefaultResources();
+
     /* Vulkan Rule
     shader->setEnvInput((Options & EOptionReadHlsl) ? glslang::EShSourceHlsl : glslang::EShSourceGlsl,
     compUnit.stage, glslang::EShClientVulkan, ClientInputSemanticsVersion);
@@ -413,7 +305,7 @@ bool ShaderCodeTranspiler::compileAndLinkFromHlsl(
             glslang::TShader preprocessor(LNStageToEShLanguage(m_stage));
             preprocessor.setStringsWithLengthsAndNames(shaderCode, shaderLenght, shaderName, 1);
 
-            if (!preprocessor.preprocess(&DefaultTBuiltInResource, defaultVersion, EProfile::ENoProfile, false, forwardCompatible, messages, &preprocessedCode, includer)) {
+            if (!preprocessor.preprocess(resource, defaultVersion, EProfile::ENoProfile, false, forwardCompatible, messages, &preprocessedCode, includer)) {
                 if (!StringHelper::isNullOrEmpty(preprocessor.getInfoLog())) diag->reportError(String::fromCString(preprocessor.getInfoLog()));
                 if (!StringHelper::isNullOrEmpty(preprocessor.getInfoDebugLog())) diag->reportError(String::fromCString(preprocessor.getInfoDebugLog()));
                 return false;
@@ -430,7 +322,7 @@ bool ShaderCodeTranspiler::compileAndLinkFromHlsl(
             {12053} normal block at 0x06EB1410, 8 bytes long.
                 Data: <k       > 6B 0F 00 00 FF FF FF FF
         */
-        if (!m_shader->parse(&DefaultTBuiltInResource, defaultVersion, forwardCompatible, messages, includer)) {
+        if (!m_shader->parse(resource, defaultVersion, forwardCompatible, messages, includer)) {
             if (!StringHelper::isNullOrEmpty(m_shader->getInfoLog())) diag->reportError(String::fromCString(m_shader->getInfoLog()));
             if (!StringHelper::isNullOrEmpty(m_shader->getInfoDebugLog())) diag->reportError(String::fromCString(m_shader->getInfoDebugLog()));
             return false;
@@ -451,7 +343,7 @@ bool ShaderCodeTranspiler::compileAndLinkFromHlsl(
             {12053} normal block at 0x06EB1410, 8 bytes long.
                 Data: <k       > 6B 0F 00 00 FF FF FF FF
         */
-        if (!m_shader->parse(&DefaultTBuiltInResource, defaultVersion, forwardCompatible, messages, includer)) {
+        if (!m_shader->parse(resource, defaultVersion, forwardCompatible, messages, includer)) {
             if (!StringHelper::isNullOrEmpty(m_shader->getInfoLog())) diag->reportError(String::fromCString(m_shader->getInfoLog()));
             if (!StringHelper::isNullOrEmpty(m_shader->getInfoDebugLog())) diag->reportError(String::fromCString(m_shader->getInfoDebugLog()));
             return false;
