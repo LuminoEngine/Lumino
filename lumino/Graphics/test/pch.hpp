9af050ccb06eb433e6e255808b1a37e6a0282716
lriki <lriki.net@gmail.com>
2024-03-27 16:33:52 +0900

    Delete old files

--- lumino/Graphics/test/pch.hpp
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include "gtest/gtest.h"
-#include <LuminoCore.hpp>
-#include <LuminoCore/Testing/TestHelper.hpp>
-#include <LuminoEngine/LuminoGraphics.hpp>
-using namespace ln;
-
-
-
