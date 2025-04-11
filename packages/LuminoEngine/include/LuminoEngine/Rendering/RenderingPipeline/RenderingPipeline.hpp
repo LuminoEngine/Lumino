fedc9a8c4731bdfcb5929c80248917069cc54c9c
lriki <lriki.net@gmail.com>
2025-04-11 23:58:41 +0900

    Remove UnLigitingSceneRendererPass

--- packages/LuminoEngine/include/LuminoEngine/Rendering/RenderingPipeline/RenderingPipeline.hpp
+++ packages/LuminoEngine/include/LuminoEngine/Rendering/RenderingPipeline/RenderingPipeline.hpp
@@ -6,8 +6,6 @@ class GraphicsCommandList;
 class RenderView;
 namespace detail {
 class ClusteredShadingSceneRenderer;
-class UnLigitingSceneRenderer;
-class UnLigitingSceneRendererPass;
 } // namespace detail
 
 /**
