02fef971ec2e2adc6cac1c8672de47cd31d7bdf2
LRIKI <lriki.net@gmail.com>
2026-03-20 16:16:21 +0900

    Add PCH

---
+++ packages/lumino_base/src/pch.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+// Standard C headers
+#include <cassert>
+#include <cmath>
+#include <cstring>
+
+// Standard C++ headers
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <vector>
---
+++ packages/lumino_core/src/pch.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+// Standard C headers
+#include <cassert>
+#include <cmath>
+#include <cstring>
+
+// Standard C++ headers
+#include <algorithm>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <string>
+#include <unordered_map>
+#include <vector>
